d8b57da56b46abcea1c8dab63d8a24b8f597ab38
Christopher Johnson <cjohn@ceh.ac.uk>
2014-05-28 16:35:30 +0100

    Adding user test

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/model/CatalogueUser.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/model/CatalogueUser.java
@@ -2,6 +2,7 @@ package uk.ac.ceh.gateway.catalogue.model;
 
 import lombok.Data;
 import uk.ac.ceh.components.datastore.DataAuthor;
+import uk.ac.ceh.components.userstore.User;
 import uk.ac.ceh.components.userstore.UserAttribute;
 import static uk.ac.ceh.components.userstore.UserAttribute.*;
 
@@ -10,7 +11,7 @@ import static uk.ac.ceh.components.userstore.UserAttribute.*;
  * @author cjohn
  */
 @Data
-public class CatalogueUser implements uk.ac.ceh.components.userstore.User, DataAuthor {
+public class CatalogueUser implements User, DataAuthor {
     public static CatalogueUser PUBLIC_USER = new CatalogueUser();
     
     private @UserAttribute(USERNAME) String username;
---
+++ java/src/test/java/uk/ac/ceh/gateway/catalogue/model/CatalogueUserTest.java
@@ -0,0 +1,37 @@
+package uk.ac.ceh.gateway.catalogue.model;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ *
+ * @author cjohn
+ */
+public class CatalogueUserTest {
+    @Test
+    public void checkUserWithNullUsernameIsPublic() {
+        //Given
+        CatalogueUser user = new CatalogueUser();
+        
+        //When
+        boolean isPublic = user.isPublic();
+        
+        //Then
+        assertTrue("No username supplied, expected to be public", isPublic);
+    }
+    
+    
+    @Test
+    public void checkUserWithUsernameIsNotPublic() {
+        //Given
+        CatalogueUser user = new CatalogueUser();
+        user.setUsername("james@bo.jones");
+        
+        //When
+        boolean isPublic = user.isPublic();
+        
+        //Then
+        assertFalse("Expected user with username to not be public", isPublic);
+    }
+}
