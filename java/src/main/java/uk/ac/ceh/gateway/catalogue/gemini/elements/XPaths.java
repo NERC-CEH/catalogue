eca5b09fb7c21932abfeeda17575abf9559cdc7d
nbnrain <jcoop@ceh.ac.uk>
2014-06-13 09:19:25 +0100

    Added description to simple object mapping from xml

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/gemini/elements/XPaths.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/gemini/elements/XPaths.java
@@ -3,6 +3,7 @@ package uk.ac.ceh.gateway.catalogue.gemini.elements;
 public class XPaths {
     public static final String ID = "/*/gmd:fileIdentifier/gco:CharacterString";
     public static final String TITLE = "/*/gmd:identificationInfo/*/gmd:citation/*/gmd:title/gco:CharacterString";
+    public static final String DESCRIPTION = "/*/gmd:identificationInfo/*/gmd:abstract/gco:CharacterString";
     public static final String ALTERNATE_TITLE = "/*/gmd:identificationInfo/*/gmd:citation/*/gmd:alternateTitle/gco:CharacterString";
     public static final String LANGUAGE_CODE_LIST = "/*/gmd:identificationInfo/gmd:language/gmd:LanguageCode/@codeList";
     public static final String LANGUAGE_CODE_LIST_VALUE = "/*/gmd:identificationInfo/gmd:language/gmd:LanguageCode/@codeListValue";
