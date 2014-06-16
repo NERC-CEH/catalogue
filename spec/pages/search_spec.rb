b5f19274d58ca0658d1b8356c7dbd972d214573d
nbnrain <jcoop@ceh.ac.uk>
2014-06-16 15:45:20 +0100

    Adding some testing for the search page

---
+++ spec/pages/search_spec.rb
@@ -0,0 +1,24 @@
+describe "Search page", :type => :feature do
+  it "should find land cover map 2007 as top result" do
+    visit "/documents"
+
+    within("#search-form") do
+      fill_in "term", :with => 'land cover map 2007'
+      click_button "Search"
+    end
+
+    expect(first('.result')).to have_content 'Land Cover Map 2007'
+  end
+
+  it "should show the search term in the search box after a search" do
+    visit "/documents"
+
+    within("#search-form") do
+      fill_in "term", :with => 'any old search term'
+      click_button "Search"
+    end
+
+    expect(find_field('term').value).to eq 'any old search term'
+  end
+
+end
