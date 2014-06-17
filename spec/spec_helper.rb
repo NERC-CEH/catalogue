0f5c47db65645af52c41fb43d82fb44e93a51a41
unknown <cjohn@LAD-2HZZ7X1.ad.nerc.ac.uk>
2014-06-17 17:08:11 +0100

    Using the google chrome driver

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,12 @@
 require 'rubygems'
 require 'capybara/rspec'
-require 'capybara/poltergeist'
+require 'selenium/webdriver'
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
 
 Capybara.app_host = 'https://localhost:8080'
 Capybara.run_server = false
-Capybara.default_driver = :poltergeist
+Capybara.default_driver = :chrome
 Capybara.default_selector = :css
