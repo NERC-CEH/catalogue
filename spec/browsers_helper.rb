fec31a65768a807d31e5b23dfbdf2252d432ea05
unknown <cjohn@LAD-2HZZ7X1.ad.nerc.ac.uk>
2014-06-19 15:45:12 +0100

    Reading from env variable, we can use different browsers by supplying details to this

--- spec/browsers_helper.rb
+++ spec/browsers_helper.rb
@@ -1,4 +1,6 @@
-BROWSERS = [:chrome]
+# Parse the environment var browsers. If it is not there just test chrome
+browsers = ENV['BROWSERS'] || 'chrome'
+BROWSERS = browsers.split(',').map {|s| s.to_sym}
 
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
