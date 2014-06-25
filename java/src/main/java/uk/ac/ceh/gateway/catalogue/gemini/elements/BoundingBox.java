7a53980de9dbc2ab9d10d592f2827720155745d8
Rod Scott <rjsc@ceh.ac.uk>
2014-06-25 12:42:38 +0100

    CEHCIG-101 extract Bounding boxes from xml
     - BoundingBoxConverter extracts multiple bounding box extents from a document
     - provide method for generating Google static map url

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/gemini/elements/BoundingBox.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/gemini/elements/BoundingBox.java
@@ -6,7 +6,9 @@ import lombok.experimental.Builder;
 
 @Value
 public class BoundingBox {
+    private static final String STATIC_MAP_BASE_URL = "https://maps.googleapis.com/maps/api/staticmap?sensor=false&size=300x300&path=color:0xAA0000FF|weight:3|";
     private final BigDecimal westBoundLongitude, eastBoundLongitude, southBoundLatitude, northBoundLatitude;
+    private final StringBuilder googleStaticMapUrl;
     
     @Builder
     private BoundingBox(String westBoundLongitude, String eastBoundLongitude, String southBoundLatitude, String northBoundLatitude) {
@@ -14,5 +16,30 @@ public class BoundingBox {
         this.eastBoundLongitude = new BigDecimal(eastBoundLongitude);
         this.southBoundLatitude = new BigDecimal(southBoundLatitude);
         this.northBoundLatitude = new BigDecimal(northBoundLatitude);
+        googleStaticMapUrl= new StringBuilder(STATIC_MAP_BASE_URL);
+    }
+
+    String getGoogleStaticMapUrl() {
+        return googleStaticMapUrl
+            .append(southBoundLatitude)
+            .append(",")
+            .append(westBoundLongitude)
+            .append("|")
+            .append(southBoundLatitude)
+            .append(",")
+            .append(eastBoundLongitude)
+            .append("|")
+            .append(northBoundLatitude)
+            .append(",")
+            .append(eastBoundLongitude)
+            .append("|")
+            .append(northBoundLatitude)
+            .append(",")
+            .append(westBoundLongitude)
+            .append("|")
+            .append(southBoundLatitude)
+            .append(",")
+            .append(westBoundLongitude)
+            .toString();
     }
 }
