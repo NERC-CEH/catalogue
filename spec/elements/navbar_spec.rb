e4276445f450a5839161cffe6b05518d9b1926b3
Christopher Johnson <cjohn@ceh.ac.uk>
2014-07-02 17:02:52 +0100

    Adding some tests for the navbar

---
+++ spec/elements/navbar_spec.rb
@@ -0,0 +1,58 @@
+BROWSERS.each do |browser|
+  describe "#{browser} rendering the navbar", :type => :feature, :driver => browser do
+    describe "toggle button" do
+      it "should show when the width is 760px" do
+        page.driver.browser.manage.window.resize_to(760,400)
+    
+        visit '/documents'
+
+        expect(page).to have_selector('.navbar-toggle')
+      end
+
+      it "should be hidden when the width is 900px" do
+        page.driver.browser.manage.window.resize_to(900,400)
+    
+        visit '/documents'
+        
+        expect(page).to_not have_selector('.navbar-toggle')
+      end
+
+      it "should expand when clicked" do
+        page.driver.browser.manage.window.resize_to(760,400)
+    
+        visit '/documents'
+        find('.navbar-toggle').click
+
+        expect(page).to have_selector('.navbar-nav')
+      end
+
+      it "should be hidden by default" do
+        page.driver.browser.manage.window.resize_to(760,400)
+    
+        visit '/documents'
+
+        expect(page).to_not have_selector('.navbar-nav')
+      end
+    end
+
+    describe "the logo" do
+      it "should be small when the width is 760px" do
+        page.driver.browser.manage.window.resize_to(760,400)
+    
+        visit '/documents'
+
+        expect(page).to have_selector('.navbar-mediaSml')
+        expect(page).to_not have_selector('.navbar-brand')
+      end
+
+      it "should be big when the width is 900px" do
+        page.driver.browser.manage.window.resize_to(900,400)
+    
+        visit '/documents'
+
+        expect(page).to_not have_selector('.navbar-mediaSml')
+        expect(page).to have_selector('.navbar-brand')
+      end
+    end
+  end
+end
