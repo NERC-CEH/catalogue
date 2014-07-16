6d6b0c2f4d9af731c55aa0129e1e441571cf82bc
Rod Scott <rjsc@ceh.ac.uk>
2014-07-16 13:36:32 +0100

    CEHCIG-134 inital work on implementing TerraCatalogDocumentInfoFactory

---
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/util/terracatalog/StateTranslatingMetadataInfoFactory.java
@@ -0,0 +1,32 @@
+package uk.ac.ceh.gateway.catalogue.util.terracatalog;
+
+import java.util.Map;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Value;
+import uk.ac.ceh.gateway.catalogue.gemini.GeminiDocument;
+import uk.ac.ceh.gateway.catalogue.gemini.MetadataInfo;
+
+@Value
+public class StateTranslatingMetadataInfoFactory implements TerraCatalogDocumentInfoFactory<MetadataInfo>{
+    @Getter(AccessLevel.NONE)
+    private final Map<String, String> stateTranslation;
+
+    public StateTranslatingMetadataInfoFactory(Map<String, String> stateTranslation) {
+        this.stateTranslation = stateTranslation;
+    }
+
+    @Override
+    public MetadataInfo getDocumentInfo(GeminiDocument document, TerraCatalogExt ext) {
+        return new MetadataInfo(ext.getOwnerGroup(), null, translate(ext.getStatus()));
+    }
+    
+    private String translate(String terraCatalogStatus) {
+        if (stateTranslation.containsKey(terraCatalogStatus)) {
+            return stateTranslation.get(terraCatalogStatus);
+        } else {
+            return "draft";
+        }
+    }
+
+}
---
+++ java/src/test/java/uk/ac/ceh/gateway/catalogue/util/terracatalog/StateTranslatingMetadataInfoFactoryTest.java
@@ -0,0 +1,30 @@
+package uk.ac.ceh.gateway.catalogue.util.terracatalog;
+
+import java.util.HashMap;
+import java.util.Map;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import uk.ac.ceh.gateway.catalogue.gemini.GeminiDocument;
+import uk.ac.ceh.gateway.catalogue.gemini.MetadataInfo;
+
+public class StateTranslatingMetadataInfoFactoryTest {
+
+    @Test
+    public void getDocumentInfo() {
+        //Given
+        Map<String, String> stateTransalation = new HashMap<>();
+        stateTransalation.put("private", "draft");
+        TerraCatalogDocumentInfoFactory<MetadataInfo> factory = new StateTranslatingMetadataInfoFactory(stateTransalation);
+        GeminiDocument document = new GeminiDocument();
+        TerraCatalogExt ext = new TerraCatalogExt("testOwner", "testGroup", "private", "protection");
+        MetadataInfo expected = new MetadataInfo("testGroup", null, "draft");
+        
+        //When
+        MetadataInfo actual = factory.getDocumentInfo(document, ext);
+        
+        //Then
+        assertThat("MetadataInfo 'actual' should equal 'expected'", actual, equalTo(expected));
+    }
+    
+}
