292237fde4eb8298955ed36973b97b382db5dbe9
Christopher Johnson <cjohn@ceh.ac.uk>
2014-07-29 15:16:40 +0100

    Adding new tests which take screenshots. Much more targeted than before

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,13 +20,3 @@ hostname = Socket.gethostbyname(Socket.gethostname).first
 Capybara.app_host = "https://#{hostname}:8080"
 Capybara.run_server = false
 Capybara.default_selector = :css
-
-# XXX: Removed global screenshot taking, should be more test specific
-#
-# Taking shapshots of all tests
-# Not too sure if this is a good idea yet but got it in to see how it goes
-# RSpec.configure do |c|
-#   c.after :each do |x|
-#     page.save_screenshot("rspec_screenshots/#{x.metadata[:full_description]}.png")
-#   end
-# end
