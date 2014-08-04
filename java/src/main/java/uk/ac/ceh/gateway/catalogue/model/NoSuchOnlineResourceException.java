48dc48a0ded5120c19f5db996882d19573e6c84c
Christopher Johnson <cjohn@ceh.ac.uk>
2014-08-04 11:30:01 +0100

    Setting default http status code for new exceptions

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/model/NoSuchOnlineResourceException.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/model/NoSuchOnlineResourceException.java
@@ -1,9 +1,13 @@
 package uk.ac.ceh.gateway.catalogue.model;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  *
  * @author cjohn
  */
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NoSuchOnlineResourceException extends IllegalArgumentException {
     public NoSuchOnlineResourceException(String mess) {
         super(mess);
