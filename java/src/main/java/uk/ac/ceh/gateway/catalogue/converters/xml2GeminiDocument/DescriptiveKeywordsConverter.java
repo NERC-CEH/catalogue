6d321c2fc43cd9840aa95fd4b6127174db32c006
Rod Scott <rjsc@ceh.ac.uk>
2014-10-14 12:41:13 +0100

    CEHCIG-273 fix descriptiveKeywordsConverter

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/converters/xml2GeminiDocument/DescriptiveKeywordsConverter.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/converters/xml2GeminiDocument/DescriptiveKeywordsConverter.java
@@ -2,6 +2,7 @@ package uk.ac.ceh.gateway.catalogue.converters.xml2GeminiDocument;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
@@ -20,7 +21,7 @@ public class DescriptiveKeywordsConverter {
     private static final String THESAURUS_TITLE = "gmd:thesaurusName/*/gmd:title/*";
     private static final String THESAURUS_DATE = "gmd:thesaurusName/*/gmd:date/*/gmd:date/gco:Date";
     private static final String THESAURUS_DATE_TYPE_VALUE = "gmd:date/*/gmd:dateType/*/@codeListValue";
-    private static final String KEYWORD_CHARACTER = "gmd:keyword/*";
+    private static final String KEYWORD_CHARACTER = "gmd:keyword/gco:CharacterString";
     private static final String KEYWORD_ANCHOR = "gmd:keyword/gmx:Anchor";
     private final XPathExpression descriptiveKeywords, type, thesaurusTitle, thesaurusDate,
         thesaurusDateTypeValue, keywordCharacter, keywordAnchor;
@@ -57,42 +58,31 @@ public class DescriptiveKeywordsConverter {
         return toReturn;
     }
     
-    private List<Keyword> getKeywordsFromDescriptiveKeywordsNode(Node descriptiveKeywordsNode) throws XPathExpressionException{
+    private List<Keyword> getKeywordsFromDescriptiveKeywordsNode(Node node) throws XPathExpressionException{
         List<Keyword> toReturn = new ArrayList<>();
-        List<Keyword> keywordsWithoutURI = getKeywordsFromCharacterString(descriptiveKeywordsNode);
-        List<Keyword> keywordsWithURI = getKeywordsURIFromDescriptiveKeywordsNode(descriptiveKeywordsNode);
-        toReturn.addAll(keywordsWithoutURI);
-        toReturn.addAll(keywordsWithURI);
+        toReturn.addAll(getKeywordsFromCharacterString(node));
+        toReturn.addAll(getKeywordsURIFromDescriptiveKeywordsNode(node));
         return toReturn;
     }
     
-    private List<Keyword> getKeywordsFromCharacterString(Node descriptiveKeywordsNode) throws XPathExpressionException {
-        List<Keyword> toReturn = new ArrayList<>();
-        List<String> keywords = NodeListConverter.getListOfStrings((NodeList) keywordCharacter.evaluate(descriptiveKeywordsNode, XPathConstants.NODESET));
-        if(keywords != null && !keywords.isEmpty()){
-            keywords.stream().forEach((keyword) -> {
-                toReturn.add(Keyword.builder()
-                    .value(keyword)
-                    .URI(null)
-                    .build()
-                );
-            });
-        }
-        return toReturn;
+    private List<Keyword> getKeywordsFromCharacterString(Node node) throws XPathExpressionException {
+        List<String> keywords = NodeListConverter.getListOfStrings((NodeList) keywordCharacter.evaluate(node, XPathConstants.NODESET));
+        return keywords.stream()
+            .map((keyword) -> { return Keyword.builder().value(keyword).build(); })
+            .collect(Collectors.toList());
     }
     
     private List<Keyword> getKeywordsURIFromDescriptiveKeywordsNode(Node descriptiveKeywordsNode) throws XPathExpressionException{
         List<Keyword> toReturn = new ArrayList<>();
-        NodeList xlinkKeywords = (NodeList) keywordAnchor.evaluate(descriptiveKeywordsNode, XPathConstants.NODESET);
-        if(xlinkKeywords != null && xlinkKeywords.getLength() > 0){
-            for(int i=0; i<xlinkKeywords.getLength(); i++){
-                Node xlinkKeyword = xlinkKeywords.item(i);
-                toReturn.add(Keyword.builder()
-                        .value(xlinkKeyword.getFirstChild().getNodeValue())
-                        .URI(xlinkKeyword.getAttributes().getNamedItem("xlink:href").getNodeValue())
-                        .build()
-                );
-            }
+        NodeList nodeList = (NodeList) keywordAnchor.evaluate(descriptiveKeywordsNode, XPathConstants.NODESET);
+
+        for(int i=0; i<nodeList.getLength(); i++){
+            Node keywordNode = nodeList.item(i);
+            toReturn.add(Keyword.builder()
+                .value(keywordNode.getFirstChild().getNodeValue())
+                .URI(keywordNode.getAttributes().getNamedItem("xlink:href").getNodeValue())
+                .build()
+            );
         }
         return toReturn;
     }  
