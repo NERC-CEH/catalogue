b3e679e41dffb3775992d64e8c8d58355eb2a06f
Christopher Johnson <cjohn@ceh.ac.uk>
2014-10-14 13:40:21 +0100

    Adding tests for checking if prev and next pages return when nessersary

--- java/src/test/java/uk/ac/ceh/gateway/catalogue/search/SearchResultsTest.java
+++ java/src/test/java/uk/ac/ceh/gateway/catalogue/search/SearchResultsTest.java
@@ -4,11 +4,14 @@ import java.util.Collections;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.util.NamedList;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import uk.ac.ceh.gateway.catalogue.model.CatalogueUser;
 
 public class SearchResultsTest {
@@ -44,4 +47,94 @@ public class SearchResultsTest {
         assertThat("Number of search results is wrong in results", searchResults.getNumFound(), equalTo(resultFound));
     }
     
+    @Test
+    public void checkThatPrevPageIsPresentOnSecondPage() {
+        //Given
+        SearchQuery query = new SearchQuery(
+            SearchQueryTest.ENDPOINT,
+            CatalogueUser.PUBLIC_USER,
+            SearchQuery.DEFAULT_SEARCH_TERM,
+            SearchQueryTest.DEFAULT_BBOX,
+            2,
+            SearchQueryTest.DEFAULT_ROWS,
+            SearchQueryTest.DEFAULT_FITLERS);
+        
+        QueryResponse response = mock(QueryResponse.class);
+        SearchResults results = new SearchResults(response, query);
+        
+        //When
+        String pageUrl = results.getPrevPage();
+        
+        //Then
+        assertNotNull("Expected a url which is not null", pageUrl);
+    }
+    
+    @Test
+    public void checkThatPrevPageIsntShownOnFirstPage() {
+        //Given
+        SearchQuery query = new SearchQuery(
+            SearchQueryTest.ENDPOINT,
+            CatalogueUser.PUBLIC_USER,
+            SearchQuery.DEFAULT_SEARCH_TERM,
+            SearchQueryTest.DEFAULT_BBOX,
+            1,
+            SearchQueryTest.DEFAULT_ROWS,
+            SearchQueryTest.DEFAULT_FITLERS);
+        
+        QueryResponse response = mock(QueryResponse.class);
+        SearchResults results = new SearchResults(response, query);
+        
+        //When
+        String pageUrl = results.getPrevPage();
+        
+        //Then
+        assertNull("Expected to not get a page url", pageUrl);
+    }
+    
+    @Test
+    public void checkThatNoNextPageIsShownWhenOnLastPage() {
+        //Given
+        SearchQuery query = new SearchQuery(
+            SearchQueryTest.ENDPOINT,
+            CatalogueUser.PUBLIC_USER,
+            SearchQuery.DEFAULT_SEARCH_TERM,
+            SearchQueryTest.DEFAULT_BBOX,
+            2,
+            20,
+            SearchQueryTest.DEFAULT_FITLERS);
+        
+        QueryResponse response = mock(QueryResponse.class);
+        SearchResults results = spy(new SearchResults(response, query));
+        doReturn(30L).when(results).getNumFound();
+        
+        //When
+        String pageUrl = results.getNextPage();
+        
+        //Then
+        assertNull("Expected to not get a page url", pageUrl);
+    }
+    
+    @Test
+    public void checkNextPageIsShownWhenThereAreMoreResults() {
+        //Given
+        SearchQuery query = new SearchQuery(
+            SearchQueryTest.ENDPOINT,
+            CatalogueUser.PUBLIC_USER,
+            SearchQuery.DEFAULT_SEARCH_TERM,
+            SearchQueryTest.DEFAULT_BBOX,
+            2,
+            20,
+            SearchQueryTest.DEFAULT_FITLERS);
+        
+        QueryResponse response = mock(QueryResponse.class);
+        SearchResults results = spy(new SearchResults(response, query));
+        doReturn(50L).when(results).getNumFound();
+        
+        //When
+        String pageUrl = results.getNextPage();
+        
+        //Then
+        assertNotNull("Expected to not get a page url", pageUrl);
+        assertThat("Expected page=3 in url", pageUrl, containsString("page=3"));
+    }
 }
