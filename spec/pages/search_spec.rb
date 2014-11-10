3df2ada1875516f3a4b3703c2ceb3e0415697112
Christopher Johnson <cjohn@ceh.ac.uk>
2014-11-10 11:15:24 +0000

    Updating the search spec so that capybara wait for result element to appear

--- spec/pages/search_spec.rb
+++ spec/pages/search_spec.rb
@@ -2,7 +2,7 @@ describe "Search page" do
   sized [:xs, :sm, :md, :lg] do
     it 'should update results when term changes' do
       visit "/documents"
-      results = find('.results').text
+      results = first('.result').text
       within(".search-form") do
         fill_in 'term', :with => 'my search'
       end
@@ -11,14 +11,14 @@ describe "Search page" do
 
     it 'should update results when going to next page' do
       visit "/documents"
-      results = find('.results').text
+      results = first('.result').text
       click_on 'Next'
       expect(page).not_to have_content results
     end
 
     it 'should update results when going to previous page' do
       visit '/documents?page=2'
-      results = find('.results').text
+      results = first('.result').text
       click_on 'Older'
       expect(page).not_to have_content results
     end
