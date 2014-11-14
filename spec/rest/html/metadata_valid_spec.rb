b8c378079995d805d3fbf536355ee9cb6cfe4012
Christopher Johnson <cjohn@ceh.ac.uk>
2014-11-14 15:51:40 +0000

    Adjusting title

--- spec/rest/html/metadata_valid_spec.rb
+++ spec/rest/html/metadata_valid_spec.rb
@@ -1,5 +1,5 @@
 describe "Metadata html generation", :retry => 1, :retry_wait => 0, :restful => true do 
-  it "should generate strictly valid html" do
+  it "should generate strictly valid html for all documents" do
     # Grab a list of the current metadata stored inside the vagrant box
     datastore = '/var/ceh-catalogue/datastore/'
     metadata = `vagrant ssh -c "ls #{datastore}*.raw"`
