b653261ce11f336222bcde8ff3c82551360571d3
Christopher Johnson <cjohn@ceh.ac.uk>
2014-11-14 15:50:30 +0000

    Defering the obtaining of the metadata from the vagrant box until testing begins

--- spec/rest/html/metadata_valid_spec.rb
+++ spec/rest/html/metadata_valid_spec.rb
@@ -1,12 +1,12 @@
-describe "Metadata html generation", :retry => 1, :retry_wait => 0, :restful => true do
-  # Grab a list of the current metadata stored inside the vagrant box
-  datastore = '/var/ceh-catalogue/datastore/'
-  metadata = `vagrant ssh -c "ls #{datastore}*.raw"`
+describe "Metadata html generation", :retry => 1, :retry_wait => 0, :restful => true do 
+  it "should generate strictly valid html" do
+    # Grab a list of the current metadata stored inside the vagrant box
+    datastore = '/var/ceh-catalogue/datastore/'
+    metadata = `vagrant ssh -c "ls #{datastore}*.raw"`
 
-  metadata.split.map {|name| name[datastore.length..-4]}.each {|id|
-    it "should generate strictly valid html for #{id}" do
+    metadata.split.map {|name| name[datastore.length..-5]}.each {|id|
       res = Nokogiri::HTML( $site["/documents/#{id}"].get ) { |config| config.strict }
       expect(res.errors).to be_empty 
-    end
-  }
+    }
+  end
 end
