57fac07c2ce53e5b2b2a4add47bb9fdd034711c9
Christopher Johnson <cjohn@ceh.ac.uk>
2015-04-27 17:14:17 +0100

    Correcting the existing metadata ids adding paging for testing the search page

--- spec/pages/search_spec.rb
+++ spec/pages/search_spec.rb
@@ -1,7 +1,7 @@
 describe "Search page" do
   sized [:xs, :sm, :md, :lg] do
     it 'should update results when term changes' do
-      visit "/documents"
+      visit "/documents?rows=2"
       results = first('.result').text
       within(".search-form") do
         fill_in 'term', :with => 'my search'
@@ -10,14 +10,14 @@ describe "Search page" do
     end
 
     it 'should update results when going to next page' do
-      visit "/documents"
+      visit "/documents?rows=2"
       results = first('.result').text
       click_on 'Next'
       expect(page).not_to have_content results
     end
 
     it 'should update results when going to previous page' do
-      visit '/documents?page=2'
+      visit '/documents?rows=2&page=2'
       results = first('.result').text
       click_on 'Previous'
       expect(page).not_to have_content results
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,7 +25,7 @@ METADATA_IDS = [
   '1d859249-e6af-48f4-9fd6-5f8401bc1e4e',
   '1e7d5e08-9e24-471b-ae37-49b477f695e3',
   '2d023ce9-6dbe-4b4f-a0cd-34768e1455ae',
-  '57f97915-8ff1-473b-8c77-2564cbd747bc',
+  '571a676f-6c32-489b-b7ec-18dcc617a9f1',
   '6795cf1b-9204-451e-99f1-f9b90658537f',
   'bb2d7874-7bf4-44de-aa43-348bd684a2fe',
   'fb495d1b-80a3-416b-8dc7-c85ed22ed1e3',
