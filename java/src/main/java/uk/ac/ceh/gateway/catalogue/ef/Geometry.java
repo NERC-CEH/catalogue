f814cbc10675e729ea0d65d3335130a50f0888d9
Christopher Johnson <cjohn@ceh.ac.uk>
2015-05-26 12:28:39 +0100

    Removing the wkt processing to google maps images from the ef schema

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/ef/Geometry.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/ef/Geometry.java
@@ -1,118 +1,16 @@
 package uk.ac.ceh.gateway.catalogue.ef;
 
-import com.google.common.base.Splitter;
-import com.spatial4j.core.context.jts.JtsSpatialContext;
-import com.spatial4j.core.io.JtsShapeReadWriter;
-import com.spatial4j.core.shape.Point;
-import com.vividsolutions.jts.io.ParseException;
 import javax.xml.bind.annotation.*;
 import lombok.Data;
 import lombok.experimental.Accessors;
-import org.slf4j.*;
 
 @Data
 @Accessors(chain = true)
-public class Geometry {
-    @XmlTransient private final StringBuilder googleStaticMapUrl 
-        = new StringBuilder("https://maps.googleapis.com/maps/api/staticmap?sensor=false&size=300x300");
-    private static final String googleMapUrl = "https://maps.google.co.uk/maps?q=";
-    private static final Logger logger = LoggerFactory.getLogger(Geometry.class);
-    private static final int DECIMAL_PLACES = 3;
-    
+public class Geometry {    
     @XmlAttribute(name = "SRS")
     private final String SRS = "urn:ogc:def:crs:EPSG::4326";
     @XmlAttribute
     private Boolean representativePoint; 
     @XmlValue
     private String value;
-    
-    public String getCentroidAsWKT() throws ParseException {
-        if (value != null) {
-            final Point centroid = getCentroid(value);
-            return String.format("POINT(%.4f %.4f)", centroid.getX(), centroid.getY());
-        } else {
-            return "";
-        }
-    }
-    
-    public String getGoogleMapUrl() throws ParseException {
-        if (value != null) {
-            final Point centroid = getCentroid(value);
-            return String.format("%s%.4f,%.4f", googleMapUrl, centroid.getY(), centroid.getX());
-        } else {
-            return "";
-        }
-    }
-    
-    private Point getCentroid(String value) {
-        final JtsShapeReadWriter shapeReadWriter = new JtsShapeReadWriter(JtsSpatialContext.GEO);
-        return shapeReadWriter.readShape(value).getCenter();
-    }
-
-    public String getStaticMapUrl() {
-        if (value != null) {
-            if (value.toLowerCase().startsWith("point")) {
-                googleStaticMapUrl.append("&zoom=14&markers=");
-                
-                int beginIndex = value.indexOf("(") + 1;
-                int endIndex = value.indexOf(" ", beginIndex);
-                String lon = fixedDecimal(value.substring(beginIndex, endIndex), DECIMAL_PLACES);
-                
-                beginIndex = endIndex + 1;
-                endIndex = value.indexOf(")", beginIndex);
-                String lat = fixedDecimal(value.substring(beginIndex, endIndex), DECIMAL_PLACES);
-
-                googleStaticMapUrl.append(lat).append(",").append(lon);
-            } else if (value.toLowerCase().startsWith("polygon")) {
-                googleStaticMapUrl.append("&path=color:0xAA0000FF|weight:3");
-                int beginIndex = value.indexOf("((") + 2;
-                int endIndex = value.indexOf(")", beginIndex);
-                extractCoordintes(value, beginIndex, endIndex);
-            } else if (value.toLowerCase().startsWith("linestring")) {
-                googleStaticMapUrl.append("&path=color:0xAA0000FF|weight:3");
-                int beginIndex = value.indexOf("(") + 1;
-                int endIndex = value.indexOf(")", beginIndex);
-                extractCoordintes(value, beginIndex, endIndex);
-            } else {
-                return "";
-            }
-        } else {
-        }
-        String mapUrl = googleStaticMapUrl.toString();
-        logger.info("Google static map url: {}", mapUrl);
-        return mapUrl;
-    }
-    
-    private void extractCoordintes(String input, int beginIndex, int endIndex) {
-        String rawCoords = input.substring(beginIndex, endIndex);
-        logger.debug(rawCoords);
-
-        for (String coordPair : Splitter.on(",").trimResults().omitEmptyStrings().split(rawCoords)) {
-            logger.debug(coordPair);
-            String[] coords = coordPair.split(" ");
-            googleStaticMapUrl
-                .append("|")
-                .append(fixedDecimal(coords[1], DECIMAL_PLACES))
-                .append(",")
-                .append(fixedDecimal(coords[0], DECIMAL_PLACES));
-        }
-    }
-    
-    private String fixedDecimal(String coord, int fixed) {
-        int decimalIndex = coord.indexOf(".");
-        if (decimalIndex > -1) {
-            String preDecimal = coord.substring(0, decimalIndex);
-            String postDecimal;
-            try {
-                postDecimal = coord.substring(decimalIndex, decimalIndex + fixed + 1);
-            } catch (StringIndexOutOfBoundsException ex) {
-                postDecimal = coord.substring(decimalIndex);
-            }
-            logger.debug("longitude: {}, preDecimal: {}, postDecimal: {}", coord, preDecimal, postDecimal);
-            return new StringBuilder(preDecimal).append(postDecimal).toString();
-        } else {
-            logger.debug("no decimal point so just return input: {}", coord);
-            return coord;
-        }
-    }
 }
