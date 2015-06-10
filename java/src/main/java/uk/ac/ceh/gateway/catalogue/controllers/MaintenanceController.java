9116bab06d450f91fa8e5e9738c616e5f2b28034
Christopher Johnson <cjohn@ceh.ac.uk>
2015-06-10 11:30:19 +0100

    Adding a maintenance response object which shows the status of the maintenance aspect of the system Testing the importing of zip files

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/controllers/MaintenanceController.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/controllers/MaintenanceController.java
@@ -5,21 +5,25 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.zip.ZipFile;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
+import uk.ac.ceh.components.datastore.DataRepository;
+import uk.ac.ceh.components.datastore.DataRepositoryException;
 import uk.ac.ceh.components.userstore.springsecurity.ActiveUser;
 import uk.ac.ceh.gateway.catalogue.indexing.DocumentIndexingException;
 import uk.ac.ceh.gateway.catalogue.indexing.DocumentIndexingService;
 import uk.ac.ceh.gateway.catalogue.linking.DocumentLinkingException;
 import uk.ac.ceh.gateway.catalogue.linking.DocumentLinkService;
 import uk.ac.ceh.gateway.catalogue.model.CatalogueUser;
+import uk.ac.ceh.gateway.catalogue.model.MaintenanceResponse;
 import uk.ac.ceh.gateway.catalogue.services.TerraCatalogImporterService;
 import uk.ac.ceh.gateway.catalogue.services.UnknownContentTypeException;
 
@@ -31,63 +35,97 @@ import uk.ac.ceh.gateway.catalogue.services.UnknownContentTypeException;
 @RequestMapping("maintenance")
 @Secured(DocumentController.MAINTENANCE_ROLE)
 public class MaintenanceController {
+    private final DataRepository<CatalogueUser> repo;
     private final DocumentIndexingService solrIndex;
     private final DocumentLinkService linkingService;
     private final TerraCatalogImporterService terraCatalogImporterService;
     
     @Autowired
-    public MaintenanceController(DocumentIndexingService solrIndex, DocumentLinkService linkingService, TerraCatalogImporterService terraCatalogImporterService) {
+    public MaintenanceController(DataRepository<CatalogueUser> repo, DocumentIndexingService solrIndex, DocumentLinkService linkingService, TerraCatalogImporterService terraCatalogImporterService) {
+        this.repo = repo;
         this.solrIndex = solrIndex;
         this.linkingService = linkingService;
         this.terraCatalogImporterService = terraCatalogImporterService;
     }
     
     @RequestMapping (method = RequestMethod.GET)
-    public String loadMaintenancePage() {
-        return "/html/maintenance.html.tpl";
+    @ResponseBody
+    public MaintenanceResponse loadMaintenancePage() {
+        MaintenanceResponse toReturn = new MaintenanceResponse();
+        toReturn.setLinked(!linkingService.isEmpty());
+        try {
+            toReturn.setIndexed(!solrIndex.isIndexEmpty());
+        } catch(DocumentIndexingException ex) {
+            toReturn.addMessage(ex.getMessage());
+        }
+        try {
+            toReturn.setLatestRevision(repo.getLatestRevision());
+        } catch(DataRepositoryException dre) {
+            toReturn.addMessage(dre.getMessage());
+        }
+        return toReturn;
     }
     
     @RequestMapping(value="/documents/reindex",
                     method = RequestMethod.POST)
-    @ResponseStatus(HttpStatus.OK)
-    public void reindexDocuments() throws DocumentIndexingException {
-        solrIndex.rebuildIndex();
+    @ResponseBody
+    public HttpEntity<MaintenanceResponse> reindexDocuments() {
+        try {
+            solrIndex.rebuildIndex();
+            return ResponseEntity.ok(loadMaintenancePage().addMessage("All documents successfully indexed"));
+        }
+        catch(DocumentIndexingException die) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(loadMaintenancePage().addMessage(die.getMessage()));
+        }
     }
     
     @RequestMapping(value="/links/reindex",
                     method = RequestMethod.POST)
-    @ResponseStatus(HttpStatus.OK)
-    public void reindexLinks() throws DocumentLinkingException {
-        linkingService.rebuildLinks();
+    @ResponseBody
+    public HttpEntity<MaintenanceResponse> reindexLinks() {
+        try {
+            linkingService.rebuildLinks();
+            return ResponseEntity.ok(loadMaintenancePage().addMessage("All documents successfully linked"));
+        } catch (DocumentLinkingException ex) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(loadMaintenancePage().addMessage(ex.getMessage()));
+        }
     }
     
     @RequestMapping(value="/terraCatalogImport",
                     method = RequestMethod.POST)
-    @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public String importTerraCatalogZip(
+    public HttpEntity<MaintenanceResponse> importTerraCatalogZip(
             @ActiveUser CatalogueUser user,
             @RequestParam("file") MultipartFile file) {
         if (file.isEmpty()) {
-            return "No file has been uploaded";
+            return ResponseEntity
+                    .badRequest()
+                    .body(loadMaintenancePage().addMessage("No file has been uploaded"));
         }
         
         try {
             Path zipFile = Files.createTempFile("terraCatalogImport", ".zip");
             try {
                 file.transferTo(zipFile.toFile());
-
-                terraCatalogImporterService
-                        .getImporter(user)
-                        .importFile(new ZipFile(zipFile.toFile()));
-                return "File imported okay";
+                try (ZipFile zip = new ZipFile(zipFile.toFile())) {
+                    terraCatalogImporterService
+                            .getImporter(user)
+                            .importFile(zip);
+                    return ResponseEntity.ok(loadMaintenancePage().addMessage("Zip has been imported succcessfully"));
+                }
             }
             finally {
                 Files.delete(zipFile);
             }
         }
         catch(IOException | UnknownContentTypeException ex) {
-            return "failed to upload" + ex.getMessage();
+            return ResponseEntity
+                    .badRequest()
+                    .body(loadMaintenancePage().addMessage(ex.getMessage()));
         }
     }
 }
--- java/src/test/java/uk/ac/ceh/gateway/catalogue/controllers/MaintenanceControllerTest.java
+++ java/src/test/java/uk/ac/ceh/gateway/catalogue/controllers/MaintenanceControllerTest.java
@@ -1,21 +1,45 @@
 package uk.ac.ceh.gateway.catalogue.controllers;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.junit.Assert.assertThat;
 import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.Matchers.any;
 import org.mockito.Mock;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.multipart.MultipartFile;
+import uk.ac.ceh.components.datastore.DataRepository;
+import uk.ac.ceh.components.datastore.DataRepositoryException;
 import uk.ac.ceh.gateway.catalogue.indexing.DocumentIndexingException;
 import uk.ac.ceh.gateway.catalogue.indexing.DocumentIndexingService;
-import uk.ac.ceh.gateway.catalogue.linking.DocumentLinkingException;
 import uk.ac.ceh.gateway.catalogue.linking.DocumentLinkService;
+import uk.ac.ceh.gateway.catalogue.linking.DocumentLinkingException;
+import uk.ac.ceh.gateway.catalogue.model.CatalogueUser;
+import uk.ac.ceh.gateway.catalogue.model.MaintenanceResponse;
 import uk.ac.ceh.gateway.catalogue.services.TerraCatalogImporterService;
+import uk.ac.ceh.gateway.catalogue.services.UnknownContentTypeException;
+import uk.ac.ceh.gateway.catalogue.util.terracatalog.TerraCatalogImporter;
 
 /**
  *
  * @author jcoop, cjohn
  */
 public class MaintenanceControllerTest {
+    @Mock DataRepository<CatalogueUser> repo;
     @Mock DocumentIndexingService indexService;
     @Mock DocumentLinkService linkingService;
     @Mock TerraCatalogImporterService terraCatalogImporterService;
@@ -25,16 +49,15 @@ public class MaintenanceControllerTest {
     @Before
     public void createMaintenanceController() {
         MockitoAnnotations.initMocks(this);
-        controller = new MaintenanceController(indexService, linkingService, terraCatalogImporterService);
+        controller = new MaintenanceController(repo, indexService, linkingService, terraCatalogImporterService);
     }
     
     @Test
     public void checkThatReindexingDelegatesToIndexService() throws DocumentIndexingException {
         //Given
         //Nothing
-        
         //When
-        controller.reindexDocuments();
+        HttpEntity<MaintenanceResponse> reindexDocuments = controller.reindexDocuments();
         
         //Then
         verify(indexService).rebuildIndex();
@@ -52,4 +75,93 @@ public class MaintenanceControllerTest {
         verify(linkingService).rebuildLinks();
     }
     
+    @Test
+    public void checkThatCanLoadMaintenancePageWhenThereRepoIsBroken() throws DataRepositoryException {
+        //Given
+        String errorMessage = "Something has gone wrong";
+        when(repo.getLatestRevision()).thenThrow(new DataRepositoryException(errorMessage));
+        
+        //When
+        MaintenanceResponse response = controller.loadMaintenancePage();
+        
+        //Then
+        assertThat("Expected one message", response.getMessages().size(), equalTo(1));
+        assertThat("Expected message to exist", response.getMessages(), contains(errorMessage));
+    }
+
+    @Test
+    public void checkThatCanLoadMaintenancePageWhenIndexingIsBroken() throws DocumentIndexingException {
+        //Given
+        String errorMessage = "Something has gone wrong";
+        when(indexService.isIndexEmpty()).thenThrow(new DocumentIndexingException(errorMessage));
+        
+        //When
+        MaintenanceResponse response = controller.loadMaintenancePage();
+        
+        //Then
+        assertThat("Expected one message", response.getMessages().size(), equalTo(1));
+        assertThat("Expected message to exist", response.getMessages(), contains(errorMessage));
+    }
+    
+    @Test
+    public void checkThatUploadsZipAsGivenUser() throws IOException, UnknownContentTypeException {
+        //Given
+        CatalogueUser currentUser = mock(CatalogueUser.class);
+        MultipartFile file = emptyZip();
+        TerraCatalogImporter importer = mock(TerraCatalogImporter.class);
+        when(terraCatalogImporterService.getImporter(currentUser)).thenReturn(importer);
+        
+        //When
+        MaintenanceResponse response = controller.importTerraCatalogZip(currentUser, file).getBody();
+        
+        //Then
+        verify(importer).importFile(any(ZipFile.class));
+        assertThat("Expected one message", response.getMessages().size(), equalTo(1));
+        assertThat("Expected message to exist", response.getMessages(), contains("Zip has been imported succcessfully"));
+    }
+    
+    @Test
+    public void checkThatErrorMessageIsReturnedWhenNoFileIsUploaded() throws DataRepositoryException {
+        //Given
+        CatalogueUser currentUser = mock(CatalogueUser.class);
+        MultipartFile file = mock(MultipartFile.class);
+        when(file.isEmpty()).thenReturn(true);
+        
+        //When
+        MaintenanceResponse response = controller.importTerraCatalogZip(currentUser, file).getBody();
+        
+        //Then
+        assertThat("Expected one message", response.getMessages().size(), equalTo(1));
+        assertThat("Expected message to exist", response.getMessages(), contains("No file has been uploaded"));
+    }
+    
+    @Test
+    public void checkThatErrorMessageIsReturnedOnFailure() throws DataRepositoryException, IOException, UnknownContentTypeException {
+        //Given
+        CatalogueUser currentUser = mock(CatalogueUser.class);
+        MultipartFile file = emptyZip();
+        TerraCatalogImporter importer = mock(TerraCatalogImporter.class);
+        when(terraCatalogImporterService.getImporter(currentUser)).thenReturn(importer);
+        doThrow(new DataRepositoryException("Failed")).when(importer).importFile(any(ZipFile.class));
+        
+        //When
+        MaintenanceResponse response = controller.importTerraCatalogZip(currentUser, file).getBody();
+        
+        //Then
+        assertThat("Expected one message", response.getMessages().size(), equalTo(1));
+        assertThat("Expected message to exist", response.getMessages(), contains("Failed"));
+    }
+    
+    private MultipartFile emptyZip() throws IOException {
+        MultipartFile file = mock(MultipartFile.class);
+        doAnswer((Answer) (InvocationOnMock invocation) -> { //Create an empty zip file to import
+            File arg = (File)invocation.getArguments()[0];
+            try (ZipOutputStream output = new ZipOutputStream(new FileOutputStream(arg))) {
+                output.closeEntry();
+                output.flush();
+            }
+            return null;
+        }).when(file).transferTo(any(File.class));
+        return file;
+    }
 }
