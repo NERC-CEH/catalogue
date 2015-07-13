3d7aca20b194ed7c5006783ea874d60ecd71405e
Christopher Johnson <cjohn@ceh.ac.uk>
2015-07-13 13:11:11 +0100

    CEHCIG-619 Missing configuration annotation on the event setup

--- catalogue/java/src/main/java/uk/ac/ceh/gateway/catalogue/config/EventWiring.java
+++ catalogue/java/src/main/java/uk/ac/ceh/gateway/catalogue/config/EventWiring.java
@@ -3,6 +3,7 @@ package uk.ac.ceh.gateway.catalogue.config;
 import com.google.common.eventbus.EventBus;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import uk.ac.ceh.gateway.catalogue.indexing.IndexingFileEventListener;
 import uk.ac.ceh.gateway.catalogue.indexing.JenaIndexingService;
 import uk.ac.ceh.gateway.catalogue.indexing.SolrIndexingService;
@@ -12,6 +13,7 @@ import uk.ac.ceh.gateway.catalogue.services.DocumentListingService;
  *
  * @author cjohn
  */
+@Configuration
 public class EventWiring {
     @Autowired EventBus bus;
     
