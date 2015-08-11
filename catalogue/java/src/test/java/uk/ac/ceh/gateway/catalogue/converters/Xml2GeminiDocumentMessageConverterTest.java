048e4bcbd8715dd2833e964e0dbd3c4fb2aecb58
Rod Scott <rjsc@ceh.ac.uk>
2015-08-11 10:12:44 +0100

    EIDCAPPSUP-3395 Ensure Strings are null if empty

--- catalogue/java/src/test/java/uk/ac/ceh/gateway/catalogue/converters/Xml2GeminiDocumentMessageConverterTest.java
+++ catalogue/java/src/test/java/uk/ac/ceh/gateway/catalogue/converters/Xml2GeminiDocumentMessageConverterTest.java
@@ -288,7 +288,7 @@ public class Xml2GeminiDocumentMessageConverterTest {
         //Given
         HttpInputMessage message = mock(HttpInputMessage.class);
         when(message.getBody()).thenReturn(getClass().getResourceAsStream("browseGraphicUrl.xml"));
-        String expected = "";
+        String expected = null;
         
         //When
         GeminiDocument document = geminiReader.readInternal(GeminiDocument.class, message);
@@ -318,7 +318,7 @@ public class Xml2GeminiDocumentMessageConverterTest {
         //Given
         HttpInputMessage message = mock(HttpInputMessage.class);
         when(message.getBody()).thenReturn(getClass().getResourceAsStream("noBrowseGraphicUrl.xml"));
-        String expected = "";
+        String expected = null;
         
         //When
         GeminiDocument document = geminiReader.readInternal(GeminiDocument.class, message);
