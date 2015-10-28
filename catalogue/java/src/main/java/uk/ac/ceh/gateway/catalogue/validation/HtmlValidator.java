65e00a2f2d0eeab5749dc05e71aa5570b407bf1d
Christopher Johnson <cjohn@ceh.ac.uk>
2015-10-28 16:28:41 +0000

    Parse HTML CEHCIG-656

--- catalogue/java/src/main/java/uk/ac/ceh/gateway/catalogue/validation/HtmlValidator.java
+++ catalogue/java/src/main/java/uk/ac/ceh/gateway/catalogue/validation/HtmlValidator.java
@@ -1,6 +1,7 @@
 package uk.ac.ceh.gateway.catalogue.validation;
 
 import java.io.InputStream;
+import java.io.OutputStream;
 import org.springframework.http.MediaType;
 import org.w3c.tidy.Tidy;
 import org.w3c.tidy.TidyMessage;
@@ -21,7 +22,7 @@ public class HtmlValidator extends AbstractDocumentValidator {
         ValidationResult toReturn = new ValidationResult();
         Tidy tidy = new Tidy();
         tidy.setMessageListener((m) -> toReturn.reject(m.getMessage(), lookup(m.getLevel())));
-        
+        tidy.parse(stream, (OutputStream)null);
         return toReturn;
     }
     
