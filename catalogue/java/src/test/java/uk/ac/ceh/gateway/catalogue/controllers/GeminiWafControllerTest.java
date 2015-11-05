844f6d7f7d4382f6c60f3e57fded12de1ece36a4
Rod Scott <rjsc@ceh.ac.uk>
2015-11-05 13:59:57 +0000

    Remove Series from WAF
    CEHCIG-664

--- catalogue/java/src/test/java/uk/ac/ceh/gateway/catalogue/controllers/GeminiWafControllerTest.java
+++ catalogue/java/src/test/java/uk/ac/ceh/gateway/catalogue/controllers/GeminiWafControllerTest.java
@@ -42,7 +42,7 @@ public class GeminiWafControllerTest {
     public void checkThatXmlExtensionIsAppendedToGeminiMetadataRecords() throws DataRepositoryException, IOException {
         //Given
         List<String> files = Arrays.asList("test1", "test2");
-        List<String> resourceTypes = new ArrayList<>(Arrays.asList("dataset","series","service"));
+        List<String> resourceTypes = new ArrayList<>(Arrays.asList("dataset", "service"));
         when(repo.getLatestRevision().getRevisionID()).thenReturn("latest");
         when(listingService.getPublicDocuments("latest", GeminiDocument.class, resourceTypes)).thenReturn(files);
                 
