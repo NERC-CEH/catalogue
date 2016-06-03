fda9b6b7a8c9d240da6629141407ada861c795b6
Phil Trembath <phtr@ceh.ac.uk>
2016-06-03 16:29:53 +0100

    Reverting the main-out.js change

--- catalogue/web/src/scripts/main-out.js
+++ catalogue/web/src/scripts/main-out.js
@@ -98,8 +98,18 @@
  */
 
 // XMLHttpRequest.js Copyright (C) 2010 Sergey Ilinsky (http://www.ilinsky.com)
-
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
 //   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 /* Copyright (c) 2006-2008 MetaCarta, Inc., published under the Clear BSD
  * license.  See http://svn.openlayers.org/trunk/openlayers/license.txt for the
