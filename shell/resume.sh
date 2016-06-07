61d4dc933976d76ea8aa7e6161c7d74c92cb7098
Christopher Johnson <cjohn@ceh.ac.uk>
2016-06-07 13:47:19 +0100

    Removing the puppet managed code

--- shell/resume.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Remove any indexes stored on the box
-rm -Rf /var/ceh-catalogue/tdb
-rm -Rf /opt/ceh-catalogue/catalogue/solr-config/documents/data/
-
-# Restart the services
-service tomcat7-solr restart
-service tomcat7-ceh-catalogue restart
