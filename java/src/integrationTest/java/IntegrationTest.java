fffddf5d53a1217d20e7196a33695a519116b4aa
Rod Scott <rjsc@ceh.ac.uk>
2018-01-05 11:19:43 +0000

    Integration tests all using DOCKER_HOST
    CEHCIG-819-location

--- java/src/integrationTest/java/IntegrationTest.java
+++ java/src/integrationTest/java/IntegrationTest.java
@@ -1,5 +1,3 @@
-package integration;
-
 import com.jayway.jsonpath.JsonPath;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -43,12 +41,10 @@ public class IntegrationTest {
     @Test
     @SneakyThrows
     public void getCapabilities() {
-        String url = "http://{host}:{port}/maps/{id}?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.1.1";
-
         ResponseEntity<String> response = template.getForEntity(
-            url,
+            "http://{host}:{port}/maps/{id}?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.1.1",
             String.class,
-            environment.getServiceHost(WEB_SERVICE, WEB_PORT),
+            DOCKER_HOST,
             environment.getServicePort(WEB_SERVICE, WEB_PORT),
             "mapserver-shapefile"
         );
@@ -59,12 +55,10 @@ public class IntegrationTest {
     @Test
     @SneakyThrows
     public void getTmsImage() {
-        String url = "http://{host}:{port}/documents/{id}/onlineResources/0/tms/1.0.0/{layer}/3/3/5.png";
-
         ResponseEntity<String> response = template.getForEntity(
-            url,
+            "http://{host}:{port}/documents/{id}/onlineResources/0/tms/1.0.0/{layer}/3/3/5.png",
             String.class,
-            environment.getServiceHost(WEB_SERVICE, WEB_PORT),
+            DOCKER_HOST,
             environment.getServicePort(WEB_SERVICE, WEB_PORT),
             "mapserver-shapefile",
             "ukdata"
@@ -76,12 +70,10 @@ public class IntegrationTest {
     @Test
     @SneakyThrows
     public void getMapImage() {
-        String url = "http://{host}:{port}/maps/{id}?SERVICE=WMS&REQUEST=GetMap&VERSION=1.1.1&LAYERS={layers}&STYLES=&FORMAT=image/png&HEIGHT=256&WIDTH=256&SRS={srs}&BBOX=0,0,700000,1300000";
-
         ResponseEntity<String> response = template.getForEntity(
-            url,
+            "http://{host}:{port}/maps/{id}?SERVICE=WMS&REQUEST=GetMap&VERSION=1.1.1&LAYERS={layers}&STYLES=&FORMAT=image/png&HEIGHT=256&WIDTH=256&SRS={srs}&BBOX=0,0,700000,1300000",
             String.class,
-            environment.getServiceHost(WEB_SERVICE, WEB_PORT),
+            DOCKER_HOST,
             environment.getServicePort(WEB_SERVICE, WEB_PORT),
             "mapserver-raster",
             "Band1",
@@ -94,15 +86,21 @@ public class IntegrationTest {
     @Test
     @SneakyThrows
     public void getCurrentPublicationState() {
-        String url = format(
-            "http://%s:%s/documents/2d023ce9-6dbe-4b4f-a0cd-34768e1455ae/publication",
-            environment.getServiceHost(WEB_SERVICE, WEB_PORT),
-            environment.getServicePort(WEB_SERVICE, WEB_PORT)
-        );
+        //Given
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(of(MediaType.APPLICATION_JSON));
-        ResponseEntity<String> response = template.exchange(url, HttpMethod.GET, new HttpEntity<>(headers), String.class);
 
+        //When
+        ResponseEntity<String> response = template.exchange(
+            "http://{host}:{port}/documents/2d023ce9-6dbe-4b4f-a0cd-34768e1455ae/publication",
+            HttpMethod.GET,
+            new HttpEntity<>(headers),
+            String.class,
+            DOCKER_HOST,
+            environment.getServicePort(WEB_SERVICE, WEB_PORT)
+        );
+
+        //Then
         assertThat("Response should be OK", response.getStatusCode().is2xxSuccessful(), is(true));
         assertThat("Should be json", response.getHeaders().getContentType().isCompatibleWith(MediaType.APPLICATION_JSON), is(true));
     }
@@ -110,19 +108,23 @@ public class IntegrationTest {
     @Test
     @SneakyThrows
     public void searchForLand() {
-        String searchUrl = format(
-            "http://%s:%s/eidc/documents?term=land",
-            environment.getServiceHost(WEB_SERVICE, WEB_PORT),
-            environment.getServicePort(WEB_SERVICE, WEB_PORT)
-        );
+        //Given
         HttpHeaders headers = new HttpHeaders();
         headers.add("Accept", "application/json");
-        ResponseEntity<String> response = template.exchange(searchUrl, HttpMethod.GET, new HttpEntity<>(headers), String.class);
 
+        //When
+        ResponseEntity<String> response = template.exchange(
+            "http://{host}:{port}/eidc/documents?term=land",
+            HttpMethod.GET,
+            new HttpEntity<>(headers),
+            String.class,
+            DOCKER_HOST,
+            environment.getServicePort(WEB_SERVICE, WEB_PORT)
+        );
+
+        //Then
         assertThat("Response should be OK", response.getStatusCode().is2xxSuccessful(), is(true));
-
         String term = JsonPath.read(response.getBody(), "$.term");
-
         assertThat("Term", term, IsEqual.equalTo("land"));
     }
 
