e74b458c29bcfcc11eec70035d8983395363f012
Luke Preston <lukep@ceh.ac.uk>
2018-03-19 10:03:42 +0000

    parallelising validation has sped it up a lot

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/upload/UploadDocumentValidator.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/upload/UploadDocumentValidator.java
@@ -26,11 +26,13 @@ public class UploadDocumentValidator {
 
     private static void unknown (String guid, File folder, UploadFiles uploadFiles) {
         val found = FileListUtils.absolutePathsTree(folder, doesNotContain(".hash"), doesNotContain(".hash"));
-        for (val filename : found) {
+        found
+        .parallelStream()
+        .forEach(filename -> {
             if (!uploadFiles.getDocuments().containsKey(filename) && !uploadFiles.getInvalid().containsKey(filename)) {
                 uploadFiles.getInvalid().put(filename, UploadFileBuilder.create(guid, folder, uploadFiles.getPhysicalLocation(), new File(filename), UploadType.UNKNOWN_FILE)); 
-            }
-        }
+            }    
+        });
     }
 
     private static void missing (File folder, UploadFiles uploadFiles) {
@@ -41,34 +43,34 @@ public class UploadDocumentValidator {
     private static void missingInvalid (File folder, UploadFiles uploadFiles) {
         val found = FileListUtils.absolutePathsTree(folder, doesNotContain(".hash"), doesNotContain(".hash"));
         val invalid = uploadFiles.getInvalid();
-        for (val entry : invalid.entrySet()) {
+        invalid.entrySet().parallelStream().forEach(entry -> {
             val filename = entry.getKey();
             val uploadFile = entry.getValue();
             if (!found.contains(filename)) {
                 uploadFile.setType(UploadType.MISSING_FILE);
             }
-        }
+        });
     }
 
     private static void missingDocuments (File folder, UploadFiles uploadFiles) {
         val found = FileListUtils.absolutePathsTree(folder, doesNotContain(".hash"), doesNotContain(".hash"));
         val documents = uploadFiles.getDocuments();
         val invalid = uploadFiles.getInvalid();
-        for (val entry : documents.entrySet()) {
+        documents.entrySet().parallelStream().forEach(entry -> {
             val filename = entry.getKey();
             val uploadFile = entry.getValue();
             if (!found.contains(filename)) {
                 uploadFile.setType(UploadType.MISSING_FILE);
                 invalid.put(filename, uploadFile);
             }
-        }
+        });
         removeInvalid(uploadFiles);
     }
 
     private static void invalidHash(UploadFiles uploadFiles) {
         val documents = uploadFiles.getDocuments();
         val invalid = uploadFiles.getInvalid();
-        for (val entry : documents.entrySet()) {
+        documents.entrySet().parallelStream().forEach(entry -> {
             val filename = entry.getKey();
             val uploadFile = entry.getValue();
             val actualHash = HashUtils.hash(new File(filename));
@@ -77,15 +79,14 @@ public class UploadDocumentValidator {
                 uploadFile.setType(UploadType.INVALID_HASH);
                 invalid.put(filename, uploadFile);
             }
-        }
+        });
         removeInvalid(uploadFiles);
     }
 
     private static void removeInvalid(UploadFiles uploadFiles) {
         val documents = uploadFiles.getDocuments();
         val invalid = uploadFiles.getInvalid();
-        for (val filename : invalid.keySet()) {
-            documents.remove(filename);
-        }
+        invalid.keySet().parallelStream()
+            .forEach(filename -> documents.remove(filename));
     }
 }
