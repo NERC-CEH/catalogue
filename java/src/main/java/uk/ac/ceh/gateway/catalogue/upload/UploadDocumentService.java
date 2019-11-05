8c80a65bca18a70e1ddd8dac68e60d02114e4241
Luke Preston <lukep@ceh.ac.uk>
2019-11-05 10:40:54 +0000

    EDS-1443 adding status filter to query

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/upload/UploadDocumentService.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/upload/UploadDocumentService.java
@@ -7,6 +7,7 @@ import java.io.PrintWriter;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -25,18 +26,10 @@ public class UploadDocumentService {
   private final Map<String, File> folders;
   private final ExecutorService threadPool;
 
-  private UploadFiles getUploadFiles(String directory, String id, ArrayNode data, ObjectNode paginationNode) {
-    val folder = String.format("%s/%s", directory, id);
-    val files = new UploadFiles();
-    val pagination = new UploadDocumentPagination();
-    pagination.setPage(paginationNode.get("page").asInt());
-    pagination.setSize(paginationNode.get("size").asInt());
-    pagination.setTotal(paginationNode.get("total").asInt());
-    files.setPagination(pagination);
-    val documents = files.getDocuments();
-    val invalid = files.getInvalid();
-    data.forEach(item -> {
-      val uploadFile = new UploadFile();
+  private static final String[] VISIBLE_STATUS = new String[]{"VALID", "MOVING_FROM", "MOVING_TO", "VALIDATING_HASH", "NO_HASH", "WRITING", "REMOVED_UNKNOWN", "ZIPPED_UNKNOWN", "ZIPPED_UNKNOWN_MISSING", "MOVED_UNKNOWN", "MOVED_UNKNOWN_MISSING", "UNKNOWN", "UNKNOWN_MISSING", "MISSING", "MISSING_UNKNOWN", "CHANGED_MTIME", "CHANGED_HASH", "INVALID"};
+
+  private UploadFile convertJSONToUploadFile(String folder, JsonNode item) {
+    val uploadFile = new UploadFile();
       val path = item.get("path").asText();
       uploadFile.setPath(path);
       val name = path.replace(String.format("/%s/", folder), "");
@@ -60,11 +53,27 @@ public class UploadDocumentService {
         uploadFile.setDestination(item.get("destination").asText());
       val status = item.get("status").asText();
       uploadFile.setType(status);
+    return uploadFile;
+  }
+
+  private UploadFiles getUploadFiles(String directory, String id, ArrayNode data, ObjectNode paginationNode) {
+    val folder = String.format("%s/%s", directory, id);
+    val files = new UploadFiles();
+    val pagination = new UploadDocumentPagination();
+    pagination.setPage(paginationNode.get("page").asInt());
+    pagination.setSize(paginationNode.get("size").asInt());
+    pagination.setTotal(paginationNode.get("total").asInt());
+    files.setPagination(pagination);
+    val documents = files.getDocuments();
+    val invalid = files.getInvalid();
+    data.forEach(item -> {
+      val uploadFile = convertJSONToUploadFile(folder, item);
+      val status = item.get("status").asText();
+      val path = item.get("path").asText();
       if (status.equals("WRITING") || status.equals("VALID") || status.equals("VALIDATING_HASH") || status.equals("NO_HASH") || status.equals("MOVING_FROM") || status.equals("MOVING_TO"))
         documents.put(path, uploadFile);
-      else if (!status.equals("REMOVED") && !status.equals("MOVED") && !status.equals("ZIPPED")) {
+      else
         invalid.put(path, uploadFile);
-      }
     });
     return files;
   }
@@ -78,12 +87,12 @@ public class UploadDocumentService {
     val dropboxFiles = new ObjectMapper().createArrayNode();
     val supportingDocumentFiles = new ObjectMapper().createArrayNode();
 
-    val dropboxRes = hubbubService.get(String.format("/dropbox/%s", id), documentsPage);
+    val dropboxRes = hubbubService.get(String.format("/dropbox/%s", id), documentsPage, VISIBLE_STATUS);
     val dropbox = (ArrayNode) dropboxRes.get("data");
     dropbox.forEach(item -> { dropboxFiles.add(item); });
     document.getUploadFiles().put("documents", getUploadFiles("dropbox", id, dropboxFiles, (ObjectNode) dropboxRes.get("pagination")));
-    
-    val eidchubRes = hubbubService.get(String.format("/eidchub/%s", id), datastorePage);
+
+    val eidchubRes = hubbubService.get(String.format("/eidchub/%s", id), datastorePage, VISIBLE_STATUS);
     val eidchub = (ArrayNode) eidchubRes.get("data");
     eidchub.forEach(item -> { eidchubFiles.add(item); });
     val eidchubUploadFiles = getUploadFiles("eidchub", id, eidchubFiles, (ObjectNode) eidchubRes.get("pagination"));
@@ -92,7 +101,7 @@ public class UploadDocumentService {
     eidchubUploadFiles.setZipped(isZipped);
     document.getUploadFiles().put("datastore", eidchubUploadFiles);
 
-    val supportingDocumentRes = hubbubService.get(String.format("/supporting-documents/%s", id), supportingDocumentsPage);
+    val supportingDocumentRes = hubbubService.get(String.format("/supporting-documents/%s", id), supportingDocumentsPage, VISIBLE_STATUS);
     val supportingDocument = (ArrayNode) supportingDocumentRes.get("data");
     supportingDocument.forEach(item -> { supportingDocumentFiles.add(item); });
     document.getUploadFiles().put("supporting-documents", getUploadFiles("supporting-documents", id, supportingDocumentFiles, (ObjectNode) supportingDocumentRes.get("pagination")));
