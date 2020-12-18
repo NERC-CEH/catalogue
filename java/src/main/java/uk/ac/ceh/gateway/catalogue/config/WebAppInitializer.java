8defca0d5cdfa14b3a7a960534660ad6a86f81eb
Rod Scott <rjsc@ceh.ac.uk>
2020-12-18 14:38:10 +0000

    Remove redundant profile declaration

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/config/WebAppInitializer.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/config/WebAppInitializer.java
@@ -1,21 +1,20 @@
 package uk.ac.ceh.gateway.catalogue.config;
 
-import javax.servlet.FilterRegistration;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
 public class WebAppInitializer implements WebApplicationInitializer {
 
     @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
+    public void onStartup(ServletContext servletContext) {
         AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-        rootContext.getEnvironment().setDefaultProfiles("production");
         rootContext.register(WebConfig.class);
         
         servletContext.addListener(new ContextLoaderListener(rootContext));
