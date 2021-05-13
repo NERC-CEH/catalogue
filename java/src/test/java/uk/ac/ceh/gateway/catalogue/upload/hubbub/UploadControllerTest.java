e71901c731e71e0386574d68e2ac0eeda4e1b328
Rod Scott <rjsc@ceh.ac.uk>
2021-05-13 16:25:57 +0100

    Filtering out whole Hubbub UploadControllerTest on CI

--- java/src/test/java/uk/ac/ceh/gateway/catalogue/upload/hubbub/UploadControllerTest.java
+++ java/src/test/java/uk/ac/ceh/gateway/catalogue/upload/hubbub/UploadControllerTest.java
@@ -5,6 +5,7 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -37,6 +38,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static uk.ac.ceh.gateway.catalogue.config.CatalogueMediaTypes.UPLOAD_DOCUMENT_JSON;
 import static uk.ac.ceh.gateway.catalogue.config.DevelopmentUserStoreConfig.UPLOADER_USERNAME;
 
+@Tag("not-on-ci")
 @Slf4j
 @ActiveProfiles({"test", "upload:hubbub"})
 @Import({SecurityConfigCrowd.class, DevelopmentUserStoreConfig.class})
