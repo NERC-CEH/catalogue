7dc9250c1794f052e5964293c3a0c53c30016790
Philip Trembath <phtr@ceh.ac.uk>
2021-09-10 14:52:02 +0100

    removed solr imports and nercmodelusecontroller

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/controllers/NercModelController.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/controllers/NercModelController.java
@@ -8,11 +8,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import uk.ac.ceh.components.userstore.springsecurity.ActiveUser;
 import uk.ac.ceh.gateway.catalogue.modelnerc.NercModel;
+import uk.ac.ceh.gateway.catalogue.modelnerc.NercModelUse;
 import uk.ac.ceh.gateway.catalogue.model.CatalogueUser;
 import uk.ac.ceh.gateway.catalogue.model.MetadataDocument;
 import uk.ac.ceh.gateway.catalogue.repository.DocumentRepository;
 
 import static uk.ac.ceh.gateway.catalogue.config.CatalogueMediaTypes.NERC_MODEL_JSON_VALUE;
+import static uk.ac.ceh.gateway.catalogue.config.CatalogueMediaTypes.NERC_MODEL_USE_JSON_VALUE;
 
 @Slf4j
 @ToString(callSuper = true)
@@ -37,4 +39,20 @@ public class NercModelController extends AbstractDocumentController {
       @RequestBody NercModel document) {
     return saveMetadataDocument(user, file, document);
   }
+
+  @PreAuthorize("@permission.userCanCreate(#catalogue)")
+  @RequestMapping(value = "documents", method = RequestMethod.POST, consumes = NERC_MODEL_USE_JSON_VALUE)
+  public ResponseEntity<MetadataDocument> newNercModelUse(@ActiveUser CatalogueUser user, @RequestBody NercModelUse document,
+      @RequestParam("catalogue") String catalogue) {
+    return saveNewMetadataDocument(user, document, catalogue, "new Model use (NERC)");
+  }
+
+  @PreAuthorize("@permission.userCanEdit(#file)")
+  @RequestMapping(value = "documents/{file}", method = RequestMethod.PUT, consumes = NERC_MODEL_USE_JSON_VALUE)
+  public ResponseEntity<MetadataDocument> updateNercModelUse(@ActiveUser CatalogueUser user, @PathVariable("file") String file,
+      @RequestBody NercModelUse document) {
+    return saveMetadataDocument(user, file, document);
+  }
+
+
 }
