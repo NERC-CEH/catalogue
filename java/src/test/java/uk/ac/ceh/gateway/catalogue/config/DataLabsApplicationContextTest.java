d66973a257834779c453a4943834dd0af85b0e2a
Rod Scott <rjsc@ceh.ac.uk>
2021-11-16 11:57:36 +0000

    Add search profile to test classes
    DUKEMS-109

--- java/src/test/java/uk/ac/ceh/gateway/catalogue/config/DataLabsApplicationContextTest.java
+++ java/src/test/java/uk/ac/ceh/gateway/catalogue/config/DataLabsApplicationContextTest.java
@@ -9,7 +9,7 @@ import uk.ac.ceh.gateway.catalogue.CatalogueWebTest;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@ActiveProfiles({"auth:datalabs", "server:datalabs"})
+@ActiveProfiles({"auth:datalabs", "server:datalabs", "search:basic"})
 @CatalogueWebTest
 @DisplayName("DataLabs production context")
 class DataLabsApplicationContextTest {
