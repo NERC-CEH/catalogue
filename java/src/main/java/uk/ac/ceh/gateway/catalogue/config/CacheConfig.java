b017250d7d84857a20e41031fcf8e8a002738f3b
Rod Scott <rjsc@ceh.ac.uk>
2021-12-20 17:00:46 +0000

    Cache configuration does not break tests
    Had to add Profile to prevent caching breaking tests when all run together.

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/config/CacheConfig.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/config/CacheConfig.java
@@ -1,28 +1,42 @@
 package uk.ac.ceh.gateway.catalogue.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
-import org.springframework.stereotype.Component;
+import lombok.val;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.jcache.JCacheCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+import javax.cache.Caching;
 import javax.cache.configuration.MutableConfiguration;
 import javax.cache.expiry.AccessedExpiryPolicy;
 import javax.cache.expiry.Duration;
 import java.util.concurrent.TimeUnit;
 
+@Profile("cache")
+@SuppressWarnings("DuplicatedCode")
 @Slf4j
-@Component
-public class CachingConfig implements JCacheManagerCustomizer {
+@Configuration
+public class CacheConfig extends CachingConfigurerSupport {
 
+    @Bean
     @Override
-    public void customize(javax.cache.CacheManager cacheManager) {
+    public CacheManager cacheManager() {
         log.info("Customizing caches");
+        val provider = Caching.getCachingProvider();
+        val cacheManager = provider.getCacheManager();
+
         cacheManager.createCache("capabilities", new MutableConfiguration<>()
-            .setExpiryPolicyFactory(AccessedExpiryPolicy.factoryOf(new Duration(TimeUnit.MINUTES, 5))));
+            .setExpiryPolicyFactory(AccessedExpiryPolicy.factoryOf(new Duration(TimeUnit.MINUTES, 30))));
         cacheManager.createCache("crowd-user", new MutableConfiguration<>()
             .setExpiryPolicyFactory(AccessedExpiryPolicy.factoryOf(new Duration(TimeUnit.MINUTES, 30))));
         cacheManager.createCache("crowd-user-groups", new MutableConfiguration<>()
             .setExpiryPolicyFactory(AccessedExpiryPolicy.factoryOf(new Duration(TimeUnit.MINUTES, 30))));
         cacheManager.createCache("metadata-listings", new MutableConfiguration<>()
             .setExpiryPolicyFactory(AccessedExpiryPolicy.factoryOf(new Duration(TimeUnit.MINUTES, 3))));
+
+        return new JCacheCacheManager(cacheManager);
     }
 }
