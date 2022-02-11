39bf3fd1c831556fd4b4491542d080a814822ceb
Thomas Staunton <thosta@ceh.ac.uk>
2022-02-11 12:26:07 +0000

    Added ChromeHeadless
    EDS-2864

--- web/permission/karma.conf.js
+++ web/permission/karma.conf.js
@@ -52,7 +52,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
-    browsers: [],
+    browsers: ['ChromeHeadless'],
 
     client: {
       clearContext: true
