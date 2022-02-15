30e05d878094f15bb748a5b774bc8a2ae1e773fd
decaffeinate <thosta@ceh.ac.uk>
2022-02-15 10:09:11 +0000

    decaffeinate: Run post-processing cleanups on EditorView.spec.coffee and 1 other file

--- web/catalogue/test/InputView.spec.js
+++ web/catalogue/test/InputView.spec.js
@@ -1,3 +1,9 @@
+/* eslint-disable
+    no-return-assign,
+    no-undef,
+*/
+// TODO: This file was created by bulk-decaffeinate.
+// Fix any style issues and re-enable lint.
 /*
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
@@ -6,14 +12,14 @@
 define([
   'cs!views/editor/InputView',
   'cs!models/EditorMetadata'
-], (InputView, EditorMetadata) => describe('InputView', function() {
-  let model = null;
-  let view = null;
+], (InputView, EditorMetadata) => describe('InputView', function () {
+  let model = null
+  let view = null
 
-  beforeEach(function() {
-    model = new EditorMetadata({title: 'some text'});
-    return view = new InputView({model});
-  });
+  beforeEach(function () {
+    model = new EditorMetadata({ title: 'some text' })
+    return view = new InputView({ model })
+  })
 
-  return describe('when view is constructing', () => it('should exist', () => expect(view).toBeDefined()));
-}));
+  return describe('when view is constructing', () => it('should exist', () => expect(view).toBeDefined()))
+}))
