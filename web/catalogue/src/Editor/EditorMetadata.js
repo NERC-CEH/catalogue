c61ce509f80d8e4c3aff56cc6f15bf019b2c32aa
decaffeinate <thosta@ceh.ac.uk>
2022-02-15 10:12:04 +0000

    decaffeinate: Convert EditorMetadata.coffee to JS

--- web/catalogue/src/Editor/EditorMetadata.js
+++ web/catalogue/src/Editor/EditorMetadata.js
@@ -1,37 +1,58 @@
-define [
-  'underscore'
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+define([
+  'underscore',
   'backbone'
-], (_, Backbone) -> Backbone.Model.extend
+], function(_, Backbone) { return Backbone.Model.extend({
 
-  url: ->
-    do @urlRoot
+  url() {
+    return (this.urlRoot)();
+  },
 
-  urlRoot: ->
-    if @isNew()
-      "/documents?catalogue=#{window.location.pathname.split('/')[1]}"
-    else
-      "/documents/#{@id}"
+  urlRoot() {
+    if (this.isNew()) {
+      return `/documents?catalogue=${window.location.pathname.split('/')[1]}`;
+    } else {
+      return `/documents/${this.id}`;
+    }
+  },
 
-  initialize: ->
-    if arguments.length > 1
-      @mediaType = arguments[1].mediaType
-      @title = arguments[2]
-    else
-      @mediaType = 'application/json'
+  initialize() {
+    if (arguments.length > 1) {
+      this.mediaType = arguments[1].mediaType;
+      return this.title = arguments[2];
+    } else {
+      return this.mediaType = 'application/json';
+    }
+  },
 
-  sync: (method, model, options)->
-    Backbone.sync.call @, method, model, _.extend options,
-      accepts:
-        json: @mediaType
-      contentType: @mediaType
+  sync(method, model, options){
+    return Backbone.sync.call(this, method, model, _.extend(options, {
+      accepts: {
+        json: this.mediaType
+      },
+      contentType: this.mediaType
+    }
+    )
+    );
+  },
 
-  validate: (attrs) ->
-    errors = []
-    unless attrs?.title?
-      errors.push 'A title is mandatory'
+  validate(attrs) {
+    const errors = [];
+    if ((attrs != null ? attrs.title : undefined) == null) {
+      errors.push('A title is mandatory');
+    }
 
-    if _.isEmpty errors
-      # return nothing from Backbone.Model.validate because returning something signals a validation error.
-      return
-    else
-      return errors
+    if (_.isEmpty(errors)) {
+      // return nothing from Backbone.Model.validate because returning something signals a validation error.
+      return;
+    } else {
+      return errors;
+    }
+  }
+});
+ });
