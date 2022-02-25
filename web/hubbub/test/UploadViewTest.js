7b2d23811caf5910f0686294243b0e936e88aa2e
Thomas Staunton <thosta@ceh.ac.uk>
2022-02-25 14:13:37 +0000

    Created UploadView tests
    EDS-2875

--- web/hubbub/test/UploadViewTest.js
+++ web/hubbub/test/UploadViewTest.js
@@ -18,4 +18,130 @@ describe('Test DropzoneView', function () {
     // then
     expect(view).toBeDefined()
   })
+
+  it('loadDatastore should be triggered', () => {
+    // given
+    view.render()
+    view.delegateEvents()
+    spyOn($, 'ajax').and.callFake(function (e) {
+      return ''
+    })
+    spyOn(view, 'loadDatastore')
+
+    // when
+    view.$('.load.datastore').trigger('click')
+
+    // then
+    setTimeout(function () {
+      expect(view.loadDatastore).toHaveBeenCalled()
+    })
+  })
+
+  it('loadDropbox should be triggered', () => {
+    // given
+    view.render()
+    view.delegateEvents()
+    spyOn($, 'ajax').and.callFake(function (e) {
+      return ''
+    })
+    spyOn(view, 'loadDropbox')
+
+    // when
+    view.$('.load.data').trigger('click')
+
+    // then
+    setTimeout(function () {
+      expect(view.loadDropbox).toHaveBeenCalled()
+    })
+  })
+
+  it('loadMetadata should be triggered', () => {
+    // given
+    view.render()
+    view.delegateEvents()
+    spyOn($, 'ajax').and.callFake(function (e) {
+      return ''
+    })
+    spyOn(view, 'loadMetadata')
+
+    // when
+    view.$('.load.metadata').trigger('click')
+
+    // then
+    setTimeout(function () {
+      expect(view.loadMetadata).toHaveBeenCalled()
+    })
+  })
+
+  it('moveAllDatastore should be triggered', () => {
+    // given
+    view.render()
+    view.delegateEvents()
+    spyOn($, 'ajax').and.callFake(function (e) {
+      return ''
+    })
+    spyOn(view, 'loadMetadata')
+
+    // when
+    view.$('.move-all').trigger('click')
+
+    // then
+    setTimeout(function () {
+      expect(view.moveAllDatastore).toHaveBeenCalled()
+    })
+  })
+
+  it('reschedule should be triggered', () => {
+    // given
+    view.render()
+    view.delegateEvents()
+    spyOn($, 'ajax').and.callFake(function (e) {
+      return ''
+    })
+    spyOn(view, 'reschedule')
+
+    // when
+    view.$('.reschedule').trigger('click')
+
+    // then
+    setTimeout(function () {
+      expect(view.reschedule).toHaveBeenCalled()
+    })
+  })
+
+  it('schedule should be triggered', () => {
+    // given
+    view.render()
+    view.delegateEvents()
+    spyOn($, 'ajax').and.callFake(function (e) {
+      return ''
+    })
+    spyOn(view, 'schedule')
+
+    // when
+    view.$('.schedule').trigger('click')
+
+    // then
+    setTimeout(function () {
+      expect(view.schedule).toHaveBeenCalled()
+    })
+  })
+
+  it('validate-all should be triggered', () => {
+    // given
+    view.render()
+    view.delegateEvents()
+    spyOn($, 'ajax').and.callFake(function (e) {
+      return ''
+    })
+    spyOn(view, 'validateAll')
+
+    // when
+    view.$('.validate-all').trigger('click')
+
+    // then
+    setTimeout(function () {
+      expect(view.validateAll).toHaveBeenCalled()
+    })
+  })
 })
