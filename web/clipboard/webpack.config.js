edbf4baec9d9c614a1824fc48902e07fed9ad23a
Thomas Staunton <thosta@ceh.ac.uk>
2022-03-01 11:00:33 +0000

    Created Clipboard directory

---
+++ web/clipboard/webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(html|tpl)$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: 8081
+  },
+  output: {
+    filename: 'clipboard-app.js'
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]jquery[\\/]/,
+          name: 'vendor'
+        }
+      }
+    }
+  }
+}
