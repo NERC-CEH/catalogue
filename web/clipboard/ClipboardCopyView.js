2c7ec635b8f4d8e3f021b6b138f77037a5a981de
decaffeinate <thosta@ceh.ac.uk>
2022-03-01 11:02:58 +0000

    decaffeinate: Run post-processing cleanups on ClipboardCopyView.coffee

--- web/clipboard/ClipboardCopyView.js
+++ web/clipboard/ClipboardCopyView.js
@@ -1,3 +1,9 @@
+/* eslint-disable
+    no-return-assign,
+    no-undef,
+*/
+// TODO: This file was created by bulk-decaffeinate.
+// Fix any style issues and re-enable lint.
 /*
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
@@ -7,52 +13,51 @@ define([
   'jquery',
   'backbone',
   'tpl!templates/ClipboardCopy.tpl'
-], 
-function($, Backbone, template) {
+],
+function ($, Backbone, template) {
+  // #
+  // # Add a <span class="clipboard-copy" data-selector="{my-selector}"></span>
+  // # to any element to copy to clipboard.
+  // # Replace {my-selector} with the selector for the element you want copied.
+  // #
 
-  //#
-  //# Add a <span class="clipboard-copy" data-selector="{my-selector}"></span>
-  //# to any element to copy to clipboard.
-  //# Replace {my-selector} with the selector for the element you want copied.
-  //#
+  const copy = function (selector) {
+    const selection = self.getSelection()
+    const range = document.createRange()
+    const copyContent = document.querySelector(selector)
 
-  const copy = function(selector) {
-      const selection = self.getSelection();
-      const range = document.createRange();
-      const copyContent = document.querySelector(selector);
-      
-      if (copyContent) {
-        range.selectNode(copyContent);
-        selection.empty();
-        selection.addRange(range);
-        document.execCommand('copy');
-        return selection.empty();
-      }
-    };
+    if (copyContent) {
+      range.selectNode(copyContent)
+      selection.empty()
+      selection.addRange(range)
+      document.execCommand('copy')
+      return selection.empty()
+    }
+  }
 
   return Backbone.View.extend({
 
     events: {
-      'click': 'copyToClipboard'
+      click: 'copyToClipboard'
     },
 
-    initialize() {
-      return (this.render)();
+    initialize () {
+      return (this.render)()
     },
 
-    render() { 
-      this.$el.html(template());
+    render () {
+      this.$el.html(template())
       this.$('button').tooltip({
         title: 'copied',
         trigger: 'click'
-      });
-      return this;
+      })
+      return this
     },
 
-    copyToClipboard(event) {
-      copy($(event.currentTarget).data('selector'));
-      self.clearTimeout(this.timeout);
-      return this.timeout = self.setTimeout((function() { return this.$('button').tooltip('hide'); }), 1000);
+    copyToClipboard (event) {
+      copy($(event.currentTarget).data('selector'))
+      self.clearTimeout(this.timeout)
+      return this.timeout = self.setTimeout(function () { return this.$('button').tooltip('hide') }, 1000)
     }
-  });
-});
+  })
+})
