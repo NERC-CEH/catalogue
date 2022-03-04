fe6d9e64a0d4ff6dd83b3805d4032a618614472d
Thomas Staunton <thosta@ceh.ac.uk>
2022-03-04 16:02:43 +0000

    saving progress
    EDS-2819

--- web/webpack/webpack.config.js
+++ web/webpack/webpack.config.js
@@ -3,8 +3,12 @@ const HtmlWebPackPlugin = require('html-webpack-plugin')
 module.exports = {
 
   entry: {
-    catalogue: './catalogue/src/index.js',
+    catalogue: {
+      import: './catalogue/src/index.js',
+      dependOn: 'editor'
+    },
     clipboard: './clipboard/src/index.js',
+    editor: './editor/src/index.js',
     hubbub: './hubbub/src/index.js',
     permission: './permission/src/index.js',
     simpleupload: './simple-upload/src/index.js'
@@ -50,6 +54,10 @@ module.exports = {
     }),
     new HtmlWebPackPlugin({
       template: './clipboard/src/index.html',
+      filename: './index.html'
+    }),
+    new HtmlWebPackPlugin({
+      template: './editor/src/index.html',
       filename: './index.html'
     }),
     new HtmlWebPackPlugin({
