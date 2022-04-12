55b8741e4f8303fad072160c54ccb1d5faa59507
decaffeinate <thosta@ceh.ac.uk>
2022-04-12 16:29:56 +0100

    decaffeinate: Convert AuthorView.coffee and 6 other files to JS

--- web/src/scripts/views/service-agreement/TextOnlyView.js
+++ web/src/scripts/views/service-agreement/TextOnlyView.js
@@ -1,17 +1,26 @@
-define [
-  'underscore'
-  'cs!views/editor/SingleView'
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+define([
+  'underscore',
+  'cs!views/editor/SingleView',
   'tpl!templates/service-agreement/TextOnly.tpl'
-], (_, SingleView, template) -> SingleView.extend 
+], function(_, SingleView, template) { return SingleView.extend({ 
 
-  template: template
+  template,
 
-  className: 'component component--textonly'
+  className: 'component component--textonly',
 
-  initialize: (options) ->
-    SingleView.prototype.initialize.call @, options
-    do @render
+  initialize(options) {
+    SingleView.prototype.initialize.call(this, options);
+    return (this.render)();
+  },
 
-  render: ->
-    SingleView.prototype.render.apply @
-    @$('.dataentry').append @template data: _.extend {}, @data, value: @model.get
+  render() {
+    SingleView.prototype.render.apply(this);
+    return this.$('.dataentry').append(this.template({data: _.extend({}, this.data, {value: this.model.get})}));
+  }
+});
+ });
