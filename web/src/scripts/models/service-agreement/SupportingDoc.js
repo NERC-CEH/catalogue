e610918c348e4a6917f3906d30770e743a0bc5c4
decaffeinate <thosta@ceh.ac.uk>
2022-04-12 16:21:59 +0100

    decaffeinate: Convert Author.coffee and 3 other files to JS

--- web/src/scripts/models/service-agreement/SupportingDoc.js
+++ web/src/scripts/models/service-agreement/SupportingDoc.js
@@ -1,7 +1,14 @@
-define [
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+define([
   'backbone'
-], (Backbone) -> Backbone.Model.extend
+], Backbone => Backbone.Model.extend({
 
-  defaults:
+  defaults: {
     content:     'Collection/generation methods\nNature and Units of recorded values\nQuality control\nDetails of data structure\nExperimental design/Sampling regime\nFieldwork and laboratory instrumentation\nCalibration steps and values\nAnalytical methods\nAny other information useful to the interpretation of the data'
+  }
+}));
 
