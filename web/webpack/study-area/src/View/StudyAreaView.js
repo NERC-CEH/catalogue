98e79e859b7e9a46a5ab7fffb86020d8afdb5dcd
thosta <thosta@ceh.ac.uk>
2022-05-13 16:01:28 +0100

    Update StudyAreaView.js

--- web/webpack/study-area/src/View/StudyAreaView.js
+++ web/webpack/study-area/src/View/StudyAreaView.js
@@ -1,106 +1,44 @@
+import Backbone from 'backbone'
 import _ from 'underscore'
-import { ObjectInputView } from '../views'
-import 'leaflet/dist/leaflet.css'
 import 'leaflet-draw/dist/leaflet.draw-src.css'
+import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
-import 'leaflet-draw'
-import template from './Geometry.tpl'
-export default ObjectInputView.extend({
-
-  events: {
-    'click #update': 'viewMap'
-  },
+import $ from 'jquery'
+export default Backbone.View.extend({
 
   initialize () {
     L.Icon.Default.imagePath = 'https://unpkg.com/leaflet-draw@1.0.2/dist/images/' // fix for leaflet draw marker bug
-    this.template = _.template(template)
     this.render()
   },
 
   createMap () {
-    this.map = new L.Map(this.$('.map')[0], { center: new L.LatLng(51.513, -0.09), zoom: 4 })
+    this.overlay = L.featureGroup()
 
-    this.drawnItems = L.featureGroup()
-    if (this.model.get('geometryString')) {
-      const parsedJson = JSON.parse(this.model.get('geometryString'))
-      this.drawButtons = false
-
-      this.geometry = L.geoJson(parsedJson)
-      this.drawnItems.addLayer(this.geometry)
-    } else {
-      this.drawButtons = true
-    }
-    this.drawControl = this.createToolbar()
-    this.drawnItems.addTo(this.map)
+    const studyArea = JSON.parse(this.getStudyArea()[0])
+    const polygon = L.geoJson(studyArea)
+    const center = polygon.getBounds().getCenter()
+    const map = new L.Map($('#studyarea-map')[0], { center: center, zoom: 4 })
 
     const baseMaps = {
       Map: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 18,
         attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-      }),
+      }).addTo(map),
       Satellite: L.tileLayer('http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}', {
         attribution: 'google'
       })
     }
 
-    L.control.layers(baseMaps, { Drawlayer: this.drawnItems }, { position: 'topright', collapsed: false }).addTo(this.map)
-
-    this.map.addControl(this.drawControl)
-    baseMaps.Map.addTo(this.map)
-
-    this.listenTo(this.map, L.Draw.Event.CREATED, function (event) {
-      const layer = event.layer
-      this.drawButtons = false
-      this.model.set('geometryString', JSON.stringify(layer.toGeoJSON()))
-      this.map.removeControl(this.drawControl)
-      this.drawControl = this.createToolbar()
-      this.map.addControl(this.drawControl)
-
-      this.drawnItems.addLayer(layer)
-    })
-
-    this.listenTo(this.map, L.Draw.Event.DELETED, function () {
-      this.model.set('geometryString', null)
-      this.drawButtons = true
-      this.map.removeControl(this.drawControl)
-      this.drawControl = this.createToolbar()
-      this.map.addControl(this.drawControl)
-    })
+    L.control.layers(baseMaps, {}, { position: 'topright', collapsed: false }).addTo(map)
+    polygon.addTo(map)
   },
 
-  createToolbar () {
-    if (this.drawButtons === true) {
-      this.deleteButton = false
-    } else {
-      this.deleteButton = true
-    }
-    return new L.Control.Draw({
-      position: 'topleft',
-      edit: {
-        featureGroup: this.drawnItems,
-        edit: false,
-        remove: this.deleteButton
-      },
-      draw: {
-        rectangle: false,
-        polygon: this.drawButtons,
-        polyline: false,
-        marker: this.drawButtons,
-        circle: false,
-        circlemarker: false
-      }
-    })
-  },
-  viewMap () {
-    if (this.map) {
-      this.map.off()
-      this.map.remove()
-    }
-    this.createMap()
+  getStudyArea () {
+    const studyArea = this.$('[dataType="geoJson"]')
+    return _.map(studyArea, el => $(el).attr('content'))
   },
 
   render () {
-    ObjectInputView.prototype.render.apply(this)
     this.createMap()
     return this
   }
