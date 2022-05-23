9731c61a35c920386194ffdd1f80b46fbb7f2d75
thosta <thosta@ceh.ac.uk>
2022-05-23 16:46:31 +0100

    Got tests to work
    EDS-2965

--- web/webpack/karma.conf.js
+++ web/webpack/karma.conf.js
@@ -1,21 +1,13 @@
 process.env.CHROME_BIN = require('puppeteer').executablePath()
 const webpackConfig = require('./webpack.config.js')
-webpackConfig.entry = {}
 
 module.exports = function (config) {
   config.set({
 
-    // frameworks to use
-    // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
-    frameworks: ['jasmine', 'webpack'],
+    frameworks: ['jasmine-ajax', 'jasmine', 'webpack'],
 
-    // list of files / patterns to load in the browser
     files: [
       './*/test/*.js'
-    ],
-
-    // list of files / patterns to exclude
-    exclude: [
     ],
 
     loggers: [
@@ -24,31 +16,18 @@ module.exports = function (config) {
       }
     ],
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://www.npmjs.com/search?q=keywords:karma-preprocessor
     preprocessors: {
       './*/test/*.js': ['webpack']
     },
 
-    // points to webpack config file for preprocessing
     webpack: webpackConfig,
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'junit'],
 
-    // web server port
-    port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
     customLaunchers: {
@@ -64,18 +43,13 @@ module.exports = function (config) {
       }
     },
 
-    // start these browsers
-    // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
     browsers: ['ChromeHeadless_no_sandbox'],
 
     client: {
       clearContext: true
     },
 
-    singleRun: true,
+    singleRun: true
 
-    // Concurrency level
-    // how many browser instances should be started simultaneously
-    concurrency: Infinity
   })
 }
