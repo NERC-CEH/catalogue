08a3f5f1a66191e224db67680975da24e7bdb6cf
decaffeinate <thosta@ceh.ac.uk>
2022-05-24 17:15:00 +0100

    decaffeinate: Run post-processing cleanups on InfrastructureCategory.coffee and 1 other file

--- web/webpack/editor/src/models/InfrastructureCategory.js
+++ web/webpack/editor/src/models/InfrastructureCategory.js
@@ -1,3 +1,8 @@
+/* eslint-disable
+    no-undef,
+*/
+// TODO: This file was created by bulk-decaffeinate.
+// Fix any style issues and re-enable lint.
 /*
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
@@ -5,56 +10,57 @@
  */
 define([
   'backbone'
-], function(Backbone) { return Backbone.Model.extend({
+], function (Backbone) {
+  return Backbone.Model.extend({
 
-  defaults: {
-    value: null
-  },
+    defaults: {
+      value: null
+    },
 
-  uris: {
-    'instrumentedSites':'http://vocabs.ceh.ac.uk/ri/instrumentedSites',
-    'surveys':'http://vocabs.ceh.ac.uk/ri/surveys',
-    'wildlifeSchemes':'http://vocabs.ceh.ac.uk/ri/wildlifeSchemes',
-    'discoveryCollections':'http://vocabs.ceh.ac.uk/ri/discoveryCollections',
-    'mobilePlatforms':'http://vocabs.ceh.ac.uk/ri/mobilePlatforms',
-    'controlledPlatforms':'http://vocabs.ceh.ac.uk/ri/controlledPlatforms',
-    'fieldPlatforms':'http://vocabs.ceh.ac.uk/ri/fieldPlatforms',
-    'labsAnalysis':'http://vocabs.ceh.ac.uk/ri/labsAnalysis',
-    'labsTest':'http://vocabs.ceh.ac.uk/ri/labsTest'
-  },
+    uris: {
+      instrumentedSites: 'http://vocabs.ceh.ac.uk/ri/instrumentedSites',
+      surveys: 'http://vocabs.ceh.ac.uk/ri/surveys',
+      wildlifeSchemes: 'http://vocabs.ceh.ac.uk/ri/wildlifeSchemes',
+      discoveryCollections: 'http://vocabs.ceh.ac.uk/ri/discoveryCollections',
+      mobilePlatforms: 'http://vocabs.ceh.ac.uk/ri/mobilePlatforms',
+      controlledPlatforms: 'http://vocabs.ceh.ac.uk/ri/controlledPlatforms',
+      fieldPlatforms: 'http://vocabs.ceh.ac.uk/ri/fieldPlatforms',
+      labsAnalysis: 'http://vocabs.ceh.ac.uk/ri/labsAnalysis',
+      labsTest: 'http://vocabs.ceh.ac.uk/ri/labsTest'
+    },
 
-  classes: {
-    'instrumentedSites':'Environmental observatories',
-    'surveys':'Environmental observatories',
-    'wildlifeSchemes':'Environmental observatories',
-    'discoveryCollections':'Environmental observatories',
-    'mobilePlatforms':'Environmental observatories',
-    'controlledPlatforms':'Environmental experiment platforms',
-    'fieldPlatforms':'Environmental experiment platforms',
-    'labsAnalysis':'Environmental analysis',
-    'labsTest':'Environmental analysis'
-  },
-  
-  descriptions: {
-    'instrumentedSites':'Instrumented sites',
-    'surveys':'Surveys',
-    'wildlifeSchemes':'Wildlife monitoring schemes',
-    'discoveryCollections':'Discovery collections',
-    'mobilePlatforms':'Mobile observing platforms',
-    'controlledPlatforms':'Controlled environment platforms',
-    'fieldPlatforms':'Field research platforms',
-    'labsAnalysis':'Analysis labs',
-    'labsTest':'Test labs'
-  },
+    classes: {
+      instrumentedSites: 'Environmental observatories',
+      surveys: 'Environmental observatories',
+      wildlifeSchemes: 'Environmental observatories',
+      discoveryCollections: 'Environmental observatories',
+      mobilePlatforms: 'Environmental observatories',
+      controlledPlatforms: 'Environmental experiment platforms',
+      fieldPlatforms: 'Environmental experiment platforms',
+      labsAnalysis: 'Environmental analysis',
+      labsTest: 'Environmental analysis'
+    },
 
-  initialize() {
-    return this.on('change:value', this.updateLimitation);
-  },
+    descriptions: {
+      instrumentedSites: 'Instrumented sites',
+      surveys: 'Surveys',
+      wildlifeSchemes: 'Wildlife monitoring schemes',
+      discoveryCollections: 'Discovery collections',
+      mobilePlatforms: 'Mobile observing platforms',
+      controlledPlatforms: 'Controlled environment platforms',
+      fieldPlatforms: 'Field research platforms',
+      labsAnalysis: 'Analysis labs',
+      labsTest: 'Test labs'
+    },
 
-  updateLimitation(model, value) {
-    this.set('uri', this.uris[value] ? this.uris[value] : null);
-    this.set('infrastructureClass', this.classes[value] ? this.classes[value] : null);
-    return this.set('description', this.descriptions[value] ? this.descriptions[value] : null);
-  }
-});
- });
+    initialize () {
+      return this.on('change:value', this.updateLimitation)
+    },
+
+    updateLimitation (model, value) {
+      this.set('uri', this.uris[value] ? this.uris[value] : null)
+      this.set('infrastructureClass', this.classes[value] ? this.classes[value] : null)
+      return this.set('description', this.descriptions[value] ? this.descriptions[value] : null)
+    }
+  })
+})
--- web/webpack/editor/src/models/InfrastructureChallenge.js
+++ web/webpack/editor/src/models/InfrastructureChallenge.js
@@ -1,3 +1,8 @@
+/* eslint-disable
+    no-undef,
+*/
+// TODO: This file was created by bulk-decaffeinate.
+// Fix any style issues and re-enable lint.
 /*
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
@@ -5,26 +10,26 @@
  */
 define([
   'backbone'
-], function(Backbone) { return Backbone.Model.extend({
+], function (Backbone) {
+  return Backbone.Model.extend({
 
-  defaults: {
-    value: ''
-  },
+    defaults: {
+      value: ''
+    },
 
-  uris: {
-    'Biodiversity': 'http://vocab.ceh.ac.uk/ri#Biodiversity',
-    'Pollution': 'http://vocab.ceh.ac.uk/ri#Pollution',
-    'Climate change resilience': 'http://vocab.ceh.ac.uk/ri#Resilience',
-    'Climate change mitigation': 'http://vocab.ceh.ac.uk/ri#Mitigation'
-  },
+    uris: {
+      Biodiversity: 'http://vocab.ceh.ac.uk/ri#Biodiversity',
+      Pollution: 'http://vocab.ceh.ac.uk/ri#Pollution',
+      'Climate change resilience': 'http://vocab.ceh.ac.uk/ri#Resilience',
+      'Climate change mitigation': 'http://vocab.ceh.ac.uk/ri#Mitigation'
+    },
 
+    initialize () {
+      return this.on('change:value', this.updateUri)
+    },
 
-  initialize() {
-    return this.on('change:value', this.updateUri);
-  },
-
-  updateUri(model, value) {
-    return this.set('uri', this.uris[value] ? this.uris[value] : '');
-  }
-});
- });
+    updateUri (model, value) {
+      return this.set('uri', this.uris[value] ? this.uris[value] : '')
+    }
+  })
+})
