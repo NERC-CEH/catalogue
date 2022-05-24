7f475c710e9e6dd911ed88a940ee47e79ca1d51b
decaffeinate <thosta@ceh.ac.uk>
2022-05-24 17:14:27 +0100

    decaffeinate: Convert InfrastructureCategoryView.coffee and 2 other files to JS

--- web/webpack/editor/src/views/InfrastructureCategoryView.js
+++ web/webpack/editor/src/views/InfrastructureCategoryView.js
@@ -1,16 +1,26 @@
-define [
-  'cs!views/editor/ObjectInputView'
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+define([
+  'cs!views/editor/ObjectInputView',
   'tpl!templates/editor/InfrastructureCategory.tpl'
-], (ObjectInputView, template) -> ObjectInputView.extend
+], function(ObjectInputView, template) { return ObjectInputView.extend({
 
-  template: template
+  template,
 
-  initialize: ->
-    ObjectInputView.prototype.initialize.apply @
-    @listenTo @model, 'change:infrastructureCategory', (model, value) ->
-      @model.set 'type', value.value
+  initialize() {
+    ObjectInputView.prototype.initialize.apply(this);
+    return this.listenTo(this.model, 'change:infrastructureCategory', function(model, value) {
+      return this.model.set('type', value.value);
+    });
+  },
 
-  render: ->
-    ObjectInputView.prototype.render.apply @
-    @$('select').val @model.get 'value'
-    @
+  render() {
+    ObjectInputView.prototype.render.apply(this);
+    this.$('select').val(this.model.get('value'));
+    return this;
+  }
+});
+ });
--- web/webpack/editor/src/views/InfrastructureOnlineLinkView.js
+++ web/webpack/editor/src/views/InfrastructureOnlineLinkView.js
@@ -1,10 +1,18 @@
-define [
-  'cs!views/editor/ObjectInputView'
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+define([
+  'cs!views/editor/ObjectInputView',
   'tpl!templates/editor/InfrastructureOnlineLink.tpl'
-], (ObjectInputView, template) -> ObjectInputView.extend
+], function(ObjectInputView, template) { return ObjectInputView.extend({
 
-  template: template
+  template,
 
-  render: ->
-    ObjectInputView.prototype.render.apply @
-    @$('select.function').val @model.get 'function'
+  render() {
+    ObjectInputView.prototype.render.apply(this);
+    return this.$('select.function').val(this.model.get('function'));
+  }
+});
+ });
