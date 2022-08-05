d2444f1d9394f5fb30020a2c52b91ffbbbfa5197
thosta <thosta@ceh.ac.uk>
2022-08-05 15:53:03 +0100

    Fixed temporal extent datepicker

--- web/scripts/editor/src/views/TemporalExtentView.js
+++ web/scripts/editor/src/views/TemporalExtentView.js
@@ -1,7 +1,6 @@
 import ObjectInputView from './ObjectInputView'
 import template from '../templates/TemporalExtent.tpl'
 import _ from 'underscore'
-import $ from 'jquery'
 import 'air-datepicker'
 import 'air-datepicker/dist/js/i18n/datepicker.en.js'
 import 'air-datepicker/dist/css/datepicker.min.css'
@@ -13,33 +12,44 @@ export default ObjectInputView.extend({
   render () {
     this.template = _.template(template)
     ObjectInputView.prototype.render.apply(this)
-    // eslint-disable-next-line no-unused-vars
-
     const that = this
-    $(document).ready(function () {
-      that.$('#input-begin').datepicker({
-        language: 'en',
-        dateFormat: 'yyyy-mm-dd',
-        startDate: new Date(that.model.get('begin')),
-        position: 'top left',
-        onSelect: function (formattedDate, date, inst) {
-          that.model.set('begin', Moment(date).format('YYYY-MM-DD'))
-          that.$('#input-begin').value = that.model.set('begin', Moment(date).format('YYYY-MM-DD'))
-        }
-      })
 
-      that.$('#input-end').datepicker({
-        language: 'en',
-        minDate: new Date(that.model.get('end')),
-        dateFormat: 'yyyy-mm-dd',
-        startDate: new Date(that.model.get('begin')),
-        position: 'top left',
-        onSelect: function (formattedDate, date, inst) {
-          that.model.set('end', Moment(date).format('YYYY-MM-DD'))
-          that.$('#input-end').value = that.model.set('end', Moment(date).format('YYYY-MM-DD'))
-        }
-      })
-      return this
+    let beginStartDate
+    let endStartDate
+    if (that.model.get('begin')) {
+      beginStartDate = new Date(that.model.get('begin'))
+    } else {
+      beginStartDate = new Date()
+    }
+
+    if (that.model.get('end')) {
+      endStartDate = new Date(that.model.get('end'))
+    } else {
+      endStartDate = beginStartDate
+    }
+
+    that.$('.input-begin').datepicker({
+      language: 'en',
+      dateFormat: 'yyyy-mm-dd',
+      startDate: beginStartDate,
+      position: 'top left',
+      onSelect: function (formattedDate, date) {
+        that.model.set('begin', Moment(date).format('YYYY-MM-DD'))
+        that.$('#input-begin').value = that.model.set('begin', Moment(date).format('YYYY-MM-DD'))
+      }
     })
+
+    that.$('.input-end').datepicker({
+      language: 'en',
+      minDate: beginStartDate,
+      dateFormat: 'yyyy-mm-dd',
+      startDate: endStartDate,
+      position: 'top left',
+      onSelect: function (formattedDate, date) {
+        that.model.set('end', Moment(date).format('YYYY-MM-DD'))
+        that.$('#input-end').value = that.model.set('end', Moment(date).format('YYYY-MM-DD'))
+      }
+    })
+    return this
   }
 })
