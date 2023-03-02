97da0cee1bb29bb0e6f29c460bb52e908bb260d5
Will Bolton <wilbol@ceh.ac.uk>
2023-03-02 16:53:00 +0000

    Draft getFullRemoteRecord in SITESImportService
    runImport() now, for the first three records in the sitemap:
    	- fetches them (getFullRemoteRecord)
    	- parses them to JSON (getFullRemoteRecord)
    	- logs some of their properties (runImport)
    
    Because some records for the SITES import shouldn't be imported, there's
    no sense converting a parsed record to an ElterDocument until we've
    checked whether it's needed or not. Otherwise we could return an
    ElterDocument directly from getFullRemoteRecord.
    
    We also update some logging to info (so it displays by default) and
    update FEED ME A STRAY CAT to a nonplaceholder value (oops).

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/imports/CatalogueImportService.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/imports/CatalogueImportService.java
@@ -1,6 +1,12 @@
 package uk.ac.ceh.gateway.catalogue.imports;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.IOException;
 import java.util.List;
+
+import org.springframework.web.client.RestClientResponseException;
 
 public interface CatalogueImportService {
     // list of record IDs/locations
@@ -15,8 +21,13 @@ public interface CatalogueImportService {
     // could be merged into getNewRecords?
     //List<AbstractMetadataDocument> getLocalRecordsToUpdate(List<???> localRecordList, List<???> remoteRecordList);
 
+    // TODO: generic return type
     // remote work
-    String getFullRemoteRecord(String recordID);
+    JsonNode getFullRemoteRecord(String recordID) throws RestClientResponseException, IOException, JsonProcessingException;
+
+    // TODO: generic return type
+    // local record
+    //ElterDocument getLocalRecord(String recordID);
 
     // will this just end up calling the controller?
     String createRecord(String recordID);
@@ -26,5 +37,5 @@ public interface CatalogueImportService {
 
     // calls all the other methods, the one to schedule
     // possible to require the @Scheduled annotation?
-    void runImport();
+    void runImport() throws RestClientResponseException, IOException, JsonProcessingException;
 }
--- java/src/main/java/uk/ac/ceh/gateway/catalogue/imports/SITESImportService.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/imports/SITESImportService.java
@@ -1,5 +1,11 @@
 package uk.ac.ceh.gateway.catalogue.imports;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,11 +22,19 @@ import javax.xml.xpath.XPathFactory;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import org.xml.sax.SAXException;
@@ -34,19 +48,24 @@ import uk.ac.ceh.gateway.catalogue.repository.DocumentRepository;
 @ToString
 public class SITESImportService implements CatalogueImportService {
     // constructor prep
-    private final DocumentRepository documentRepository;
     private final DocumentBuilder documentBuilder;
+    private final DocumentRepository documentRepository;
+    private final ObjectMapper objectMapper;
+    private final RestTemplate restTemplate;
     private final XPath xPath;
 
     // constructor
     public SITESImportService(
-            DocumentRepository documentRepository
-    ) throws ParserConfigurationException {
+            DocumentRepository documentRepository,
+            @Qualifier("normal") RestTemplate restTemplate
+            ) throws ParserConfigurationException {
         log.info("Creating");
-        this.documentRepository = documentRepository;
-        this.xPath = XPathFactory.newInstance().newXPath();
         this.documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-    }
+        this.documentRepository = documentRepository;
+        this.restTemplate = restTemplate;
+        this.xPath = XPathFactory.newInstance().newXPath();
+        this.objectMapper = new ObjectMapper();
+            }
 
     // methods start here
     public List<String> getRemoteRecordList() {
@@ -54,7 +73,7 @@ public class SITESImportService implements CatalogueImportService {
         String SITESRecordExpression = "/urlset/url/loc";
         List<String> results = new ArrayList<String>();
 
-        log.debug("GET SITES sitemap at {}", SITESSitemapURL);
+        log.info("GET SITES sitemap at {}", SITESSitemapURL);
 
         try {
             XPathExpression SITESRecordXPath = xPath.compile(SITESRecordExpression);
@@ -65,16 +84,54 @@ public class SITESImportService implements CatalogueImportService {
                 results.add(recordList.item(i).getTextContent());
             }
         } catch (SAXException | IOException ex) {
-            log.error("Error retrieving sitemap");
+            log.error("Error retrieving sitemap - aborting...");
+            // need to actually abort somehow
         } catch (XPathExpressionException ex) {
-            log.error("FEED ME A STRAY CAT");
+            log.error("This should never happen: the XPathExpression is fixed and valid.");
         }
         return results;
     }
 
-    // dummy
-    public String getFullRemoteRecord(String recordID){
-        return "hello";
+    // can we use a generic or something here? along the lines of
+    // public AbstractMetadataDocument getFullRemoteRecord(String recordID)
+    public JsonNode getFullRemoteRecord(String recordID) throws RestClientResponseException, IOException, JsonProcessingException {
+        log.info("GET record {}", recordID);
+        // get record HTML as String
+        // in the SITES sitemap, recordID is the URL to the record
+        HttpHeaders headers = new HttpHeaders();
+        ResponseEntity<String> response = restTemplate.exchange(
+                recordID,
+                HttpMethod.GET,
+                new HttpEntity<>(headers),
+                String.class
+                );
+        String recordAsString = response.getBody();
+
+        // extract ld+json string from HTML
+        //
+        // Yes, this isn't a stable way to retrieve the content of a tag...
+        // but the XML isn't well-formed so until they fix it this will have to do
+        BufferedReader reader = new BufferedReader(new StringReader(recordAsString));
+        StringBuilder builder = new StringBuilder();
+        String line = null;
+        boolean started = false;
+
+        while((line=reader.readLine()) != null){
+            if(line.contains("<script type=\"application/ld+json\">")){
+                started = true;
+                continue;
+            }
+            if(started){
+                if(line.contains("</script>")){
+                    break;
+                }
+                builder.append(line);
+            }
+        }
+        String recordJSONString = builder.toString();
+
+        // parse ld+json string and return
+        return objectMapper.readTree(recordJSONString);
     }
 
     // dummy
@@ -88,11 +145,20 @@ public class SITESImportService implements CatalogueImportService {
     }
 
     @Scheduled(initialDelay = TimeConstants.ONE_MINUTE, fixedDelay = TimeConstants.SEVEN_DAYS)
-    public void runImport() {
+    public void runImport() throws RestClientResponseException, IOException, JsonProcessingException {
+        log.info("Running SITES metadata import...");
         // get sitemap
-        List<String> test = getRemoteRecordList();
-        for (int i=0; i<5; i++){
-            log.info(test.get(i));
+        List<String> remoteRecordList = getRemoteRecordList();
+        // do only 3 records while testing
+        for (int i=0; i<3; i++){
+            // get record
+            String recordID = remoteRecordList.get(i);
+            JsonNode parsedRecord = getFullRemoteRecord(recordID);
+
+            // log some info to console as proof-of-concept
+            log.info(parsedRecord.get("@type").asText());
+            log.info(parsedRecord.get("identifier").asText());
+            log.info(parsedRecord.get("description").asText());
         }
     }
 }
