ec6d4e4f833c3009bc327d27ae6ed42d6e462394
Will Bolton <wilbol@ceh.ac.uk>
2023-05-02 16:03:16 +0100

    Fix tests

--- java/src/test/java/uk/ac/ceh/gateway/catalogue/deims/DeimsSiteSearchControllerTest.java
+++ java/src/test/java/uk/ac/ceh/gateway/catalogue/deims/DeimsSiteSearchControllerTest.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WithMockCatalogueUser
-@ActiveProfiles({"elter", "test"})
+@ActiveProfiles({"server:elter", "test"})
 @DisplayName("DeimsSiteSearchController")
 @Import({SecurityConfigCrowd.class, DevelopmentUserStoreConfig.class})
 @WebMvcTest(
