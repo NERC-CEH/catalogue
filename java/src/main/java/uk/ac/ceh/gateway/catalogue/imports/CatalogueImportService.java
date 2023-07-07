26877fc0b99ff397118f3e3b4303b526690ce6d9
Will Bolton <wilbol@ceh.ac.uk>
2023-07-07 16:26:34 +0100

    Require profile for imports interface
    This should hopefully prevent accidental omission of the imports profile
    on any future import.

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/imports/CatalogueImportService.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/imports/CatalogueImportService.java
@@ -1,5 +1,8 @@
 package uk.ac.ceh.gateway.catalogue.imports;
 
+import org.springframework.context.annotation.Profile;
+
+@Profile("imports")
 public interface CatalogueImportService {
     void runImport();
 }
