582236ab0c43cec0af344f2bc0369d1a24614cd2
Philip Trembath <phtr@ceh.ac.uk>
2023-12-05 11:24:25 +0000

    updated keywords and elter projcts in ttl

--- web/src/editor/src/templates/ElterProject.js
+++ web/src/editor/src/templates/ElterProject.js
@@ -3,6 +3,7 @@ import _ from 'underscore'
 export default _.template(`
 <select data-name="value" class="editor-input">
   <option value="" selected >- Add project -</option>
+  <option value="Bio-DT">Bio-DT</option>
   <option value="e-shape">e-Shape</option>
   <option value="TA-RA project">TA-RA project</option>
 </select>
