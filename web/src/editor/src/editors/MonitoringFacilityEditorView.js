a4337d02b4666aa9e37aaaade8556cca467f6e89
Rod Scott <rjsc@ceh.ac.uk>
2024-02-02 17:27:50 +0000

    Javascript Standard fixes
    EMC-84

--- web/src/editor/src/editors/MonitoringFacilityEditorView.js
+++ web/src/editor/src/editors/MonitoringFacilityEditorView.js
@@ -1,108 +1,108 @@
 import EditorView from '../EditorView'
 import InputView from '../InputView'
 import {
-    KeywordView,
-    ParentView,
-    RelationshipView,
-    SingleObjectView,
-    TemporalExtentView,
-    TextareaView
+  KeywordView,
+  ParentView,
+  RelationshipView,
+  SingleObjectView,
+  TemporalExtentView,
+  TextareaView
 } from '../views'
-import {MultipleDate} from '../models'
+import { MultipleDate } from '../models'
 import Geometry from '../geometryMap/Geometry'
 import GeometryView from '../geometryMap/GeometryView'
 
 export default EditorView.extend({
 
-    initialize() {
-        if (!this.model.has('type')) {
-            this.model.set('type', 'monitoringFacility')
-        }
+  initialize () {
+    if (!this.model.has('type')) {
+      this.model.set('type', 'monitoringFacility')
+    }
 
-        this.sections = [{
-            label: 'Basic Info',
-            title: 'Basic Info',
-            views: [
+    this.sections = [{
+      label: 'Basic Info',
+      title: 'Basic Info',
+      views: [
 
-                new InputView({
-                    model: this.model,
-                    modelAttribute: 'title',
-                    label: 'Title',
-                    helpText: `
+        new InputView({
+          model: this.model,
+          modelAttribute: 'title',
+          label: 'Title',
+          helpText: `
 <p>Name of Monitoring Facility</p>
 `
-                }),
+        }),
 
-                new TextareaView({
-                    model: this.model,
-                    modelAttribute: 'description',
-                    rows: 13,
-                    label: 'Description',
-                    helpText: `
+        new TextareaView({
+          model: this.model,
+          modelAttribute: 'description',
+          rows: 13,
+          label: 'Description',
+          helpText: `
 <p>Description of Monitoring Facility</p>
 `
-                }),
+        }),
 
-                new SingleObjectView({
-                    model: this.model,
-                    modelAttribute: 'geometry',
-                    ModelType: Geometry,
-                    label: 'Geometry',
-                    ObjectInputView: GeometryView,
-                    helpText: `
+        new SingleObjectView({
+          model: this.model,
+          modelAttribute: 'geometry',
+          ModelType: Geometry,
+          label: 'Geometry',
+          ObjectInputView: GeometryView,
+          helpText: `
 <p>Geometry of Monitoring Facility</p>
 `
-                }),
+        }),
 
-                new InputView({
-                    model: this.model,
-                    modelAttribute: 'facilityType',
-                    label: 'Type',
-                    helpText: `
+        new InputView({
+          model: this.model,
+          modelAttribute: 'facilityType',
+          label: 'Type',
+          helpText: `
 <p>Type of Monitoring Facility</p>
 `
-                }),
+        }),
 
-                new SingleObjectView({
-                    model: this.model,
-                    modelAttribute: 'temporalExtent',
-                    ModelType: MultipleDate,
-                    label: 'Temporal Extent',
-                    ObjectInputView: TemporalExtentView,
-                    helpText: `
+        new SingleObjectView({
+          model: this.model,
+          modelAttribute: 'temporalExtent',
+          ModelType: MultipleDate,
+          label: 'Temporal Extent',
+          ObjectInputView: TemporalExtentView,
+          helpText: `
 <p>Temporal Extent of Monitoring Facility</p>
 `
-                }),
+        }),
 
-                new ParentView({
-                    model: this.model,
-                    modelAttribute: 'keywords',
-                    label: 'Keywords',
-                    ObjectInputView: KeywordView,
-                    helpText: `
+        new ParentView({
+          model: this.model,
+          modelAttribute: 'keywords',
+          label: 'Keywords',
+          ObjectInputView: KeywordView,
+          helpText: `
 <p>Keywords for discovery</p>
 `
-                }),
+        }),
 
-                new ParentView({
-                    model: this.model,
-                    modelAttribute: 'relationships',
-                    label: 'Relationships',
-                    ObjectInputView: RelationshipView,
-                    multiline: true,
-                    options: [
-                        {value: 'http://onto.nerc.ac.uk/CEHMD/rels/partOf', label: 'Part of'},
-                        {value: 'http://onto.nerc.ac.uk/CEHMD/rels/produces', label: 'Produces'}
-                    ],
-                    helpText: `
+        new ParentView({
+          model: this.model,
+          modelAttribute: 'relationships',
+          label: 'Relationships',
+          ObjectInputView: RelationshipView,
+          multiline: true,
+          options: [
+            { value: 'http://onto.nerc.ac.uk/CEHMD/rels/partOf', label: 'Part of' },
+            { value: 'http://onto.nerc.ac.uk/CEHMD/rels/produces', label: 'Produces' }
+          ],
+          helpText: `
 <p>Relationships to other OSDP document types</p>
 `
-                })
+        })
 
-            ]
-        }
-        ]
-
-        return EditorView.prototype.initialize.apply(this)
+      ]
     }
+    ]
+
+    return EditorView.prototype.initialize.apply(this)
+  }
 })
