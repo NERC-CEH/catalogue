597e4cb0c54700b81e58faa70d4b5f95d58a9823
Jonathan Cooper <jcoop@ceh.ac.uk>
2024-02-07 12:16:40 +0000

    feature: the point or polygon of a Monitoring Facility is displayed on the metadata page emc-45

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/osdp/ObservationCapability.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/osdp/ObservationCapability.java
@@ -3,6 +3,7 @@ package uk.ac.ceh.gateway.catalogue.osdp;
 import lombok.Data;
 import uk.ac.ceh.gateway.catalogue.model.Link;
 
+//TODO this should be deprecate and removed as OSDP no longer used
 @Data
 public class ObservationCapability {
     private String observingTime;
