872562683076b4e8dce08b4fa7d64730e701f256
Iain Walmsley <iainw@ceh.ac.uk>
2024-02-15 13:49:38 +0000

    feat: add relatedRecords to jena index for monitoringFacility EMC-99

--- java/src/test/java/uk/ac/ceh/gateway/catalogue/indexing/jena/JenaIndexMonitoringFacilityGeneratorTest.java
+++ java/src/test/java/uk/ac/ceh/gateway/catalogue/indexing/jena/JenaIndexMonitoringFacilityGeneratorTest.java
@@ -1,57 +1,86 @@
 package uk.ac.ceh.gateway.catalogue.indexing.jena;
 
+import lombok.val;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.rdf.model.Statement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.ac.ceh.gateway.catalogue.gemini.Geometry;
+import uk.ac.ceh.gateway.catalogue.gemini.RelatedRecord;
 import uk.ac.ceh.gateway.catalogue.monitoring.MonitoringFacility;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import static org.apache.jena.rdf.model.ResourceFactory.*;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static uk.ac.ceh.gateway.catalogue.indexing.jena.Ontology.EIDCUSES;
+import static uk.ac.ceh.gateway.catalogue.indexing.jena.Ontology.IDENTIFIER;
 
 @ExtendWith(MockitoExtension.class)
 public class JenaIndexMonitoringFacilityGeneratorTest {
     @InjectMocks private JenaIndexMonitoringFacilityGenerator service;
     @Mock private JenaIndexMetadataDocumentGenerator generator;
+    private String baseUri = "baseUri";
+    private MonitoringFacility document;
+    @BeforeEach
+    void setUp() {
+        document = mock(MonitoringFacility.class);
+        Resource monitoringFacilityResource = ResourceFactory.createResource("http://monitoringFacility");
+        when(generator.resource(document.getId())).thenReturn(monitoringFacilityResource);
+        service = new JenaIndexMonitoringFacilityGenerator(generator, baseUri);
+    }
+
+    @Test
+    void geometryShouldNotBeIndexedWhenNotProvided() {
+        //Given
+
+        //When
+        List<Statement> actual = service.generateIndex(document);
+
+        //Then
+        assertThat("Should be no statements if no indexed fields present", actual.size(), equalTo(0));
+    }
 
     @Test
     void geometryShouldBeIndexedWhenProvided() {
         //Given
-        MonitoringFacility document = mock(MonitoringFacility.class);
         String exampleGeometry = "{\"type\":\"Feature\",\"properties\":{},\"geometry\":{\"type\":\"Point\",\"coordinates\":[-2.76856,58.56252]}}";
-        when(document.getGeometry()).thenReturn(Geometry.builder().value(exampleGeometry).build());
+        when(document.getGeometry()).thenReturn(Geometry.builder().geometryString(exampleGeometry).build());
 
-        Resource monitoringFacilityResource = ResourceFactory.createResource("http://monitoringFacility");
-        when(generator.resource(document.getId())).thenReturn(monitoringFacilityResource);
+        //When
+        List<Statement> actual = service.generateIndex(document);
+
+        //Then
+        assertThat("Should be one identifier statement", actual.size(), equalTo(1));
+        assertThat("Geometry statement should be as expected", actual.get(0).getLiteral().getString(), equalTo(exampleGeometry));
+    }
+
+    @Test
+    void relatedRecordsShouldBeIndexedWhenProvided() {
+        //Given
+        String relatedUrl = "https://example.com/related";
+        String relatedId = "1";
+        String documentUrl = "https://example.com/document/1";
+        String title = "title";
+        String description = "something";
+        RelatedRecord relatedRecord = new RelatedRecord(relatedUrl, relatedId, documentUrl, title, description);
+        when(document.getRelatedRecords()).thenReturn(Collections.singletonList(relatedRecord));
 
         //When
         List<Statement> actual = service.generateIndex(document);
 
         //Then
         assertThat("Should be one identifier statements", actual.size(), equalTo(1));
-        assertThat("Geometry statement should be as expected", actual.get(0).getLiteral().getString(), equalTo(exampleGeometry));
-    }
-
-    @Test
-    void geometryShouldNotBeIndexedWhenNotProvided() {
-        //Given
-        MonitoringFacility document = mock(MonitoringFacility.class);
-        Resource monitoringFacilityResource = ResourceFactory.createResource("http://monitoringFacility");
-        when(generator.resource(document.getId())).thenReturn(monitoringFacilityResource);
-
-        //When
-        List<Statement> actual = service.generateIndex(document);
-
-        //Then
-        assertThat("Should be no statements if Geometry not present", actual.size(), equalTo(0));
+        assertThat("Geometry statement should be as expected", actual.get(0).getPredicate().toString(), equalTo(relatedUrl));
     }
 }
