3e5bed0eddf1dd5c87d5c5274a12e6aca5ccf51e
Rod Scott <rjsc@ceh.ac.uk>
2024-03-15 17:17:06 +0000

    Initial thoughts on testing Monitoring Freemarker templates
    EMC-117

---
+++ java/src/test/java/uk/ac/ceh/gateway/catalogue/monitoring/templates/MonitoringFacilityTemplateTest.java
@@ -0,0 +1,27 @@
+package uk.ac.ceh.gateway.catalogue.monitoring.templates;
+
+import lombok.val;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.tdb.TDBFactory;
+import org.junit.jupiter.api.Test;
+
+import java.io.StringReader;
+
+public class MonitoringFacilityTemplateTest {
+
+    @Test
+    void loadFacility() {
+        //given
+        val jenaTdb = TDBFactory.createDataset();
+        val model = jenaTdb.getDefaultModel();
+        val reader = new StringReader("hello");
+        val base = "http://base";
+        RDFDataMgr.read(model, reader, base, Lang.TTL);
+
+        //when
+
+
+        //then
+    }
+}
