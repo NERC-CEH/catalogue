b2c44e1e99d87401f967fa894c1a767544ad9dc9
Jonathan Cooper <jcoop@ceh.ac.uk>
2024-03-18 18:48:50 +0000

    feat: added zoom level dependent polygon to point and some tests

--- web/src/study-area/src/View/StudyAreaView.js
+++ web/src/study-area/src/View/StudyAreaView.js
@@ -6,12 +6,10 @@ import L from 'leaflet'
 export default Backbone.View.extend({
 
   initialize () {
-    // TODO: check if this is still needed
-    // L.Icon.Default.imagePath = 'https://unpkg.com/leaflet-draw@1.0.2/dist/images/' // fix for leaflet draw marker bug
     this.render()
   },
 
-  createMap () {
+  createMap: function () {
     const studyArea = JSON.parse(this.getStudyArea()[0])
     const feature = L.geoJson(studyArea)
     const map = new L.Map($('#studyarea-map')[0], { center: feature.getBounds().getCenter() })
@@ -24,35 +22,51 @@ export default Backbone.View.extend({
         attribution: 'google'
       })
     }
-
     L.control.layers(baseMaps, {}, { position: 'topright', collapsed: false }).addTo(map)
-    feature.addTo(map)
-    map.on('zoomend', function() {
-      const zoomThreshold = 16
-      const zoomFeature = map.getBoundsZoom(feature.getBounds())
-      const zoomMap = map.getZoom()
-      console.log(zoomThreshold)
-      console.log(zoomFeature)
-      console.log(zoomMap)
-      // if(zoomMap < zoomThreshold){
-      if(zoomMap < zoomThreshold && zoomFeature > zoomThreshold){
-        console.log('everything is a point')
-      } else {
-        console.log('points and polygons')
-      }
-    }, map)
     map.fitBounds(feature.getBounds())
+    feature.addTo(map)
+
+    // Zoom out when feature is a point
     if (studyArea.geometry.type.toLowerCase() === 'point') {
       map.setZoom(9)
     }
-    if (studyArea.geometry.type.toLowerCase() === 'polygon') {
-      console.log(map.getBoundsZoom(feature.getBounds()))
-    }
+
+    // Add zoom dependent point display for polygons
+    this.polygonDisplay(feature, studyArea, map)
   },
 
   getStudyArea () {
     const studyArea = this.$('[dataType="geoJson"]')
     return _.map(studyArea, el => $(el).attr('content'))
+  },
+
+  // If this is a polygon, then set a zoom threshold whereby, zooming-out will show
+  // the polygon's centroid as a point marker, and zooming-in will show the actual
+  // polygon.  This zoom threshold is '3 map zoom-outs' beyond the feature's bounding
+  // box zoom level.
+  polygonDisplay (feature, studyArea, map) {
+    const isPolygon = studyArea.geometry.type.toLowerCase() === 'polygon'
+    if (isPolygon) {
+      let zoomThreshold = map.getBoundsZoom(feature.getBounds()) - 3
+      zoomThreshold = zoomThreshold < 0 ? 0 : zoomThreshold
+      const centroidCoords = L.marker(feature.getBounds().getCenter())
+      const centroid = L.geoJson({
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [centroidCoords._latlng.lng, centroidCoords._latlng.lat]
+        }
+      })
+      map.on('zoomend', function () {
+        if (map.getZoom() < zoomThreshold) {
+          if (map.hasLayer(feature)) feature.remove()
+          if (!map.hasLayer(centroid)) centroid.addTo(map)
+        } else {
+          if (!map.hasLayer(feature)) feature.addTo(map)
+          if (map.hasLayer(centroid)) centroid.remove()
+        }
+      })
+    }
   },
 
   render () {
---
+++ web/src/study-area/test/StudyAreaViewTest.js
@@ -0,0 +1,38 @@
+import $ from 'jquery'
+import { StudyAreaView } from '../src/View'
+
+describe('Test StudyAreaView', function () {
+  let el
+
+  afterEach(() => el.remove())
+
+  it('renders point', () => {
+    // Given
+    const templatePoint = '<div id="studyarea-map"><span content="{&quot;type&quot;:&quot;Feature&quot;,&quot;properties&quot;:{},&quot;geometry&quot;:{&quot;type&quot;:&quot;Point&quot;,&quot;coordinates&quot;:[-2.64501,54.5261]}}" dataType="geoJson"/></div>'
+
+    // When
+    el = $(templatePoint).appendTo($('body'))
+    const view = new StudyAreaView({
+      el
+    })
+
+    // Then
+    expect(view).toBeDefined()
+    expect(el.hasClass('leaflet-container')).toBeTrue()
+  })
+
+  it('renders polygon', () => {
+    // Given
+    const templatePolygon = '<div id="studyarea-map"><span content="{&quot;type&quot;:&quot;Feature&quot;,&quot;properties&quot;:{},&quot;geometry&quot;:{&quot;type&quot;:&quot;Polygon&quot;,&quot;coordinates&quot;:[[[-2.64866,54.52705],[-2.64363,54.52698],[-2.64406,54.52419],[-2.64872,54.52449],[-2.64866,54.52705]]]}}" dataType="geoJson"/></div>'
+
+    // When
+    el = $(templatePolygon).appendTo($('body'))
+    const view = new StudyAreaView({
+      el
+    })
+
+    // Then
+    expect(view).toBeDefined()
+    expect(el.hasClass('leaflet-container')).toBeTrue()
+  })
+})
