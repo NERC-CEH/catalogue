3b2bd696bfb30bcf756fd891f082aa88d9856da2
Iain Walmsley <iainw@ceh.ac.uk>
2024-03-19 12:48:03 +0000

    feat: adding cluster map
    fix: correct error and update package.json

--- web/src/study-area/src/View/StudyAreaView.js
+++ web/src/study-area/src/View/StudyAreaView.js
@@ -2,6 +2,7 @@ import _ from 'underscore'
 import $ from 'jquery'
 import Backbone from 'backbone'
 import L from 'leaflet'
+import 'leaflet.markercluster/dist/leaflet.markercluster.js'
 
 export default Backbone.View.extend({
 
@@ -24,15 +25,24 @@ export default Backbone.View.extend({
     }
     L.control.layers(baseMaps, {}, { position: 'topright', collapsed: false }).addTo(map)
     map.fitBounds(feature.getBounds())
-    feature.addTo(map)
 
-    // Zoom out when feature is a point
-    if (studyArea.geometry.type.toLowerCase() === 'point') {
-      map.setZoom(9)
+    switch (studyArea.type.toLowerCase()) {
+      case 'feature': {
+        feature.addTo(map)
+        const geometryType = studyArea.geometry.type.toLowerCase()
+        if (geometryType === 'point') {
+          this.pointDisplay(feature, studyArea, map)
+        } else if (geometryType === 'polygon') {
+          this.polygonDisplay(feature, studyArea, map)
+        }
+        break
+      }
+      case 'featurecollection':
+        this.featureCollectionDisplay(feature, studyArea, map)
+        break
+      default:
+        console.log('Unknown geoJSON type.')
     }
-
-    // Add zoom dependent point display for polygons
-    this.polygonDisplay(feature, studyArea, map)
   },
 
   getStudyArea () {
@@ -40,32 +50,63 @@ export default Backbone.View.extend({
     return _.map(studyArea, el => $(el).attr('content'))
   },
 
+  pointDisplay (feature, studyArea, map) {
+    map.setZoom(9)
+  },
+
   // If this is a polygon, then set a zoom threshold whereby, zooming-out will show
   // the polygon's centroid as a point marker, and zooming-in will show the actual
   // polygon.  This zoom threshold is '3 map zoom-outs' beyond the feature's bounding
   // box zoom level.
   polygonDisplay (feature, studyArea, map) {
-    const isPolygon = studyArea.geometry.type.toLowerCase() === 'polygon'
-    if (isPolygon) {
-      let zoomThreshold = map.getBoundsZoom(feature.getBounds()) - 3
-      zoomThreshold = zoomThreshold < 0 ? 0 : zoomThreshold
-      const centroidCoords = L.marker(feature.getBounds().getCenter())
-      const centroid = L.geoJson({
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [centroidCoords._latlng.lng, centroidCoords._latlng.lat]
-        }
-      })
-      map.on('zoomend', function () {
-        if (map.getZoom() < zoomThreshold) {
-          if (map.hasLayer(feature)) feature.remove()
-          if (!map.hasLayer(centroid)) centroid.addTo(map)
-        } else {
-          if (!map.hasLayer(feature)) feature.addTo(map)
-          if (map.hasLayer(centroid)) centroid.remove()
-        }
-      })
+    let zoomThreshold = map.getBoundsZoom(feature.getBounds()) - 3
+    zoomThreshold = zoomThreshold < 0 ? 0 : zoomThreshold
+    const centroid = L.geoJson(this.centerPointOfPolygon(feature))
+
+    map.on('zoomend', function () {
+      if (map.getZoom() < zoomThreshold) {
+        if (map.hasLayer(feature)) feature.remove()
+        if (!map.hasLayer(centroid)) centroid.addTo(map)
+      } else {
+        if (!map.hasLayer(feature)) feature.addTo(map)
+        if (map.hasLayer(centroid)) centroid.remove()
+      }
+    })
+  },
+
+  featureCollectionDisplay (feature, studyArea, map) {
+    const numberOfLayers = Object.keys(feature._layers).length
+
+    // Convert all features in feature collection to points in order to play
+    // nicely with Leaflet.ClusterMap library
+    const pointFeatureCollection = studyArea.features.map(feature => {
+      if (feature.geometry.type === 'Polygon') {
+        const polygon = L.geoJSON(feature.geometry)
+        return this.centerPointOfPolygon(polygon)
+      } else {
+        return feature
+      }
+    })
+    const featureInPoints = L.geoJson(pointFeatureCollection)
+
+    if (numberOfLayers === 1) {
+      this.pointDisplay(feature, studyArea, map)
+    } else {
+      const markers = L.markerClusterGroup()
+      markers.addLayer(featureInPoints)
+      map.addLayer(markers)
+    }
+  },
+
+  // TODO: Consider returning centroid of polygon within the polygon itself
+  centerPointOfPolygon (polygon) {
+    const centroidCoords = L.marker(polygon.getBounds().getCenter())
+    return {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [centroidCoords._latlng.lng, centroidCoords._latlng.lat]
+      }
     }
   },
 
