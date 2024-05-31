4f630e1caf4c02d2f028a9ca893c9a3774cf67e3
Josh Holland <joshol@ceh.ac.uk>
2024-05-31 10:16:18 +0100

    Add ONE_HOUR to TimeConstants and make them easier to read

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/TimeConstants.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/TimeConstants.java
@@ -1,7 +1,8 @@
 package uk.ac.ceh.gateway.catalogue;
 
 public class TimeConstants {
-    public static final int ONE_MINUTE = 60000;
-    public static final int ONE_DAY = 86400000;
-    public static final int SEVEN_DAYS = 604800000;
+    public static final int ONE_MINUTE = 60_000;
+    public static final int ONE_HOUR = 60 * ONE_MINUTE;
+    public static final int ONE_DAY = 24 * ONE_HOUR;
+    public static final int SEVEN_DAYS = 7 * ONE_DAY;
 }
