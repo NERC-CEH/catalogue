fd866164ee88303f3bb7a14236984be49b94ba18
Rex Lai <rexlai@ceh.ac.uk>
2024-07-26 16:17:16 +0100

    feat: Add report query method in MetricsService. DCDEV-92

---
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/metrics/ReportMapper.java
@@ -0,0 +1,21 @@
+package uk.ac.ceh.gateway.catalogue.metrics;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReportMapper implements RowMapper<Map<String,String>> {
+    @Override
+    public Map<String, String> mapRow(ResultSet rs, int map) throws SQLException {
+        HashMap<String, String> row = new HashMap<>();
+        row.put("document", rs.getString("DOCUMENT"));
+        row.put("doc_title", rs.getString("DOC_TITLE"));
+        row.put("record_type", rs.getString("RECORD_TYPE"));
+        row.put("views", String.valueOf(rs.getInt("VIEWS")));
+        row.put("downloads", String.valueOf(rs.getInt("DOWNLOADS")));
+        return row;
+    }
+}
--- java/src/test/java/uk/ac/ceh/gateway/catalogue/metrics/JDBCMetricsServiceTest.java
+++ java/src/test/java/uk/ac/ceh/gateway/catalogue/metrics/JDBCMetricsServiceTest.java
@@ -9,8 +9,9 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.stream.IntStream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -128,5 +129,79 @@ class JDBCMetricsServiceTest {
             rows.add(List.of(rs.getString(1), rs.getInt(2)));
         }
         return rows;
+    }
+
+    @Test
+    void testGetMetricsReport() throws Exception {
+        Statement statement = db.getConnection().createStatement();
+        String sql = "insert into %s (start_timestamp, end_timestamp, document, amount, doc_title, record_type) values ('%s', '%s', '%s', %d, '%s', '%s')";
+        statement.executeUpdate(String.format(sql, "views", "1721948400", "1722034799", "abcd1", 1, "test1", "a")); // 26July2024 timestsmp
+        statement.executeUpdate(String.format(sql, "views", "1722034800", "1722121199", "abcd2", 2, "test2", "b")); // 27July2024 timestsmp
+        statement.executeUpdate(String.format(sql, "views", "1722121200", "1722207599", "abcd3", 3, "test3", "c")); // 28July2024 timestsmp
+
+        statement.executeUpdate(String.format(sql, "downloads", "1721948400", "1722034799", "abcd2", 1, "test2", "b")); // 26July2024 timestsmp
+        statement.executeUpdate(String.format(sql, "downloads", "1722034800", "1722121199", "abcd3", 2, "test3", "c")); // 27July2024 timestsmp
+        statement.executeUpdate(String.format(sql, "downloads", "1722121200", "1722207599", "abcd4", 3, "test4", "d")); // 28July2024 timestsmp
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        List<String> recordType = Arrays.asList("a", "c");
+        String orderBy = "views";
+        String ordering = "descending";
+        String docId = "abcd1";
+        Integer noOfRecords = 1;
+
+        //when
+        String result = service.getMetricsReport(df.parse("2024-07-29 00:00:00"), df.parse("2024-07-29 23:59:59"), orderBy, ordering, recordType, docId, noOfRecords).toString();
+        //then
+        assertThat(result, equalTo("[]"));  // no record in supply date
+
+        Date startDate = df.parse("2024-07-26 00:00:00");
+        Date endDate = df.parse("2024-07-27 23:59:59");
+        result = service.getMetricsReport(startDate, null, null, null, null, null, null).toString();
+        assertThat(result, equalTo("[" +
+            "{downloads=0, doc_title=test1, document=abcd1, record_type=a, views=1}, " +
+            "{downloads=1, doc_title=test2, document=abcd2, record_type=b, views=2}, " +
+            "{downloads=2, doc_title=test3, document=abcd3, record_type=c, views=3}, " +
+            "{downloads=3, doc_title=test4, document=abcd4, record_type=d, views=0}]"));
+
+        result = service.getMetricsReport(startDate, endDate, null, null, null, null, null).toString();
+        assertThat(result, equalTo("[" +
+            "{downloads=0, doc_title=test1, document=abcd1, record_type=a, views=1}, " +
+            "{downloads=1, doc_title=test2, document=abcd2, record_type=b, views=2}, " +
+            "{downloads=2, doc_title=test3, document=abcd3, record_type=c, views=0}]"));
+
+        result = service.getMetricsReport(startDate, endDate, orderBy, null, null, null, null).toString();
+        assertThat(result, equalTo("[" +
+            "{downloads=2, doc_title=test3, document=abcd3, record_type=c, views=0}, " +
+            "{downloads=0, doc_title=test1, document=abcd1, record_type=a, views=1}, " +
+            "{downloads=1, doc_title=test2, document=abcd2, record_type=b, views=2}]"));
+
+        result = service.getMetricsReport(startDate, endDate, orderBy, ordering, null, null, null).toString();
+        assertThat(result, equalTo("[" +
+            "{downloads=1, doc_title=test2, document=abcd2, record_type=b, views=2}, " +
+            "{downloads=0, doc_title=test1, document=abcd1, record_type=a, views=1}, " +
+            "{downloads=2, doc_title=test3, document=abcd3, record_type=c, views=0}]"));
+
+        orderBy = "downloads";
+        result = service.getMetricsReport(startDate, endDate, orderBy, ordering, null, null, null).toString();
+        assertThat(result, equalTo("[" +
+            "{downloads=2, doc_title=test3, document=abcd3, record_type=c, views=0}, " +
+            "{downloads=1, doc_title=test2, document=abcd2, record_type=b, views=2}, " +
+            "{downloads=0, doc_title=test1, document=abcd1, record_type=a, views=1}]"));
+
+        result = service.getMetricsReport(startDate, endDate, orderBy, ordering, recordType, null, null).toString();
+        assertThat(result, equalTo("[" +
+            "{downloads=2, doc_title=test3, document=abcd3, record_type=c, views=0}, " +
+            "{downloads=0, doc_title=test1, document=abcd1, record_type=a, views=1}]"));
+
+        result = service.getMetricsReport(startDate, endDate, orderBy, ordering, recordType, docId, null).toString();
+        assertThat(result, equalTo("[" +
+            "{downloads=0, doc_title=test1, document=abcd1, record_type=a, views=1}]"));
+
+        docId = "abcd";
+        result = service.getMetricsReport(startDate, endDate, orderBy, ordering, recordType, docId, noOfRecords).toString();
+        assertThat(result, equalTo("[" +
+            "{downloads=2, doc_title=test3, document=abcd3, record_type=c, views=0}]"));
+
     }
 }
