c445240d70c7e222c1f2b6c73696cec2058bbac9
Hamayoon Khan <hamkha@ceh.ac.uk>
2024-09-18 12:16:20 +0100

    Fix responsiveness issues with suggested keyword table, improve UI

--- web/src/editor/src/templates/FetchKeywordsButton.js
+++ web/src/editor/src/templates/FetchKeywordsButton.js
@@ -2,8 +2,8 @@ import _ from 'underscore'
 
 export default _.template(`
 <div class="col-sm-3" style="margin-top: 10px;">
-    <div class="pull-right">
-      <button class="btn btn-default legilo-fetch-btn">Suggested Keywords</button>
+    <div>
+      <button class="editor-button legilo-fetch-btn">Suggest Keywords</button>
 
       <div class="loader text-center" style="display: none; margin-top: 10px;">
         <span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span> Loading...
