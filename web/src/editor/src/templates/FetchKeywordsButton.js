b1dbad8c97782ca5757cf06ce6816185cedffe21
Philip Trembath <phtr@ceh.ac.uk>
2024-10-04 10:31:26 +0000

    updated freemarker templates for monitoring documents

--- web/src/editor/src/templates/FetchKeywordsButton.js
+++ web/src/editor/src/templates/FetchKeywordsButton.js
@@ -1,11 +1,10 @@
 import _ from 'underscore'
 
 export default _.template(`
-<div class="col-sm-3" style="margin-top: 10px;">
-    <div>
+<div class="col-sm-3" >
+    <div class="legilo">
       <button class="editor-button legilo-fetch-btn">Suggest Keywords</button>
-
-      <div class="loader text-center" style="display: none; margin-top: 10px;">
+      <div class="loader text-center">
         <span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span> Loading...
       </div>
     </div>
