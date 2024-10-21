a0677cdbb07e6270265d4f7bc13da76796c2ec7a
Hamayoon Khan <hamkha@ceh.ac.uk>
2024-10-21 15:08:12 +0100

    Add null check for facilityType to ensure MonitoringFacility gets indexed even without a type

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/indexing/solr/MonitoringDocumentUtil.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/indexing/solr/MonitoringDocumentUtil.java
@@ -4,8 +4,8 @@ import uk.ac.ceh.gateway.catalogue.gemini.TimePeriod;
 
 public class MonitoringDocumentUtil {
     public static String getTimeRange(TimePeriod timePeriod) {
-        String begin = timePeriod.getBegin() != null? timePeriod.getBegin().toString() : "*";
-        String end = timePeriod.getEnd() != null? timePeriod.getEnd().toString() : "*";
+        String begin = timePeriod.getBegin() != null ? timePeriod.getBegin().toString() : "*";
+        String end = timePeriod.getEnd() != null ? timePeriod.getEnd().toString() : "*";
         return String.format("[%s TO %s]", begin, end);
     }
 }
