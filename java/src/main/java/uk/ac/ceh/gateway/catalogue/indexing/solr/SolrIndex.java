9bf10daa44eb9fb53948cd14db12587e263ff2db
Hamayoon Khan <hamkha@ceh.ac.uk>
2024-10-25 12:38:27 +0100

    Index ObservedProperty value to Solr Indexing for searching

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/indexing/solr/SolrIndex.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/indexing/solr/SolrIndex.java
@@ -47,6 +47,7 @@ public class SolrIndex {
     private @Field List<String> keywordsProject;
     private @Field List<String> keywordsTheme;
     private @Field List<String> keywordsOther;
+    private @Field List<String> observedProperties;
     private @Field String licence;
     private @Field String lineage;
     private @Field String dataLevel;
--- java/src/main/java/uk/ac/ceh/gateway/catalogue/indexing/solr/SolrIndexGeminiDocumentGenerator.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/indexing/solr/SolrIndexGeminiDocumentGenerator.java
@@ -6,6 +6,7 @@ import uk.ac.ceh.gateway.catalogue.gemini.Keyword;
 import uk.ac.ceh.gateway.catalogue.gemini.Funding;
 import uk.ac.ceh.gateway.catalogue.gemini.GeminiDocument;
 import uk.ac.ceh.gateway.catalogue.gemini.ResourceIdentifier;
+import uk.ac.ceh.gateway.catalogue.model.ObservedProperty;
 import uk.ac.ceh.gateway.catalogue.model.Supplemental;
 import uk.ac.ceh.gateway.catalogue.indexing.IndexGenerator;
 import uk.ac.ceh.gateway.catalogue.model.ResponsibleParty;
@@ -54,6 +55,7 @@ public class SolrIndexGeminiDocumentGenerator implements IndexGenerator<GeminiDo
             .setAuthorRor(grab(document.getAuthors(), ResponsibleParty::getOrganisationIdentifier))
             .setKeywordsInstrument(grab(document.getKeywordsInstrument(), Keyword::getValue))
             .setKeywordsObservedProperty(grab(document.getKeywordsObservedProperty(), Keyword::getValue))
+            .setObservedProperties(grab(document.getObservedProperty(), ObservedProperty::getValue))
             .setKeywordsPlace(grab(document.getKeywordsPlace(), Keyword::getValue))
             .setKeywordsProject(grab(document.getKeywordsProject(), Keyword::getValue))
             .setKeywordsTheme(grab(document.getKeywordsTheme(), Keyword::getValue))
