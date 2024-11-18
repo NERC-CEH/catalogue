7af719f5fcbd8b90ee70315732b50f67d800490e
Philip Trembath <phtr@ceh.ac.uk>
2024-11-18 17:12:08 +0000

    fix dl-row class

--- web/src/editor/src/templates/LinkDocument.js
+++ web/src/editor/src/templates/LinkDocument.js
@@ -5,8 +5,8 @@ export default _.template(`
     <a href="/documents/<%=identifier%>" target="_new"><%=title%></a> <small><span class="fa-solid fa-external-link-alt" aria-hidden="true"></span></small>
 </h2>
 <div class="row">
-    <div class="col-sm-10 col-lg-10"><%=shortenedDescription%></div>
-    <div class="col-sm-2 col-lg-2">
+    <div class="col-sm-10"><%=shortenedDescription%></div>
+    <div class="col-sm-2">
         <button class="btn btn-success">Select</button>
     </div>
 </div>
--- web/src/editor/src/templates/MapDataSource.js
+++ web/src/editor/src/templates/MapDataSource.js
@@ -50,13 +50,13 @@ export default _.template(`
     <div class="col-lg-3">
         <div class="byte-box">
             <div class="row">
-                <div class="col-sm-3 col-lg-3">
+                <div class="col-sm-3">
                     <label class="control-label">Byte?</label>&nbsp;
                 </div>
-                <div class="col-sm-5 col-lg-5">
+                <div class="col-sm-5">
                     <input data-name="bytetype" type="radio" name="bytetype" value="true"  <%= data.disabled%>> Yes
                 </div>
-                <div class="col-sm-4 col-lg-4">
+                <div class="col-sm-4">
                     <input data-name="bytetype" type="radio" name="bytetype" value="false"  <%= data.disabled%>> No
                 </div>
             </div>
