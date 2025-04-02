ff7ff3619e795a211987ac6847d105a57ab69d6f
Philip Trembath <phtr@ceh.ac.uk>
2025-04-02 15:51:29 +0000

    fix css bugs

--- web/src/editor/src/models/service-agreement/Author.js
+++ web/src/editor/src/models/service-agreement/Author.js
@@ -8,7 +8,7 @@ export default Backbone.Model.extend({
   },
 
   validate ({ organisationName, email, familyName, givenName, nameIdentifier }) {
-    const emailRegEx = '^[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?'
+    const emailRegEx = '^[a-zA-Z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?'
     const orcidRegEx = '^https?:\\/\\/orcid.org\\/(\\d{4}-){3}\\d{3}[\\dX]$'
 
     const errors = []
