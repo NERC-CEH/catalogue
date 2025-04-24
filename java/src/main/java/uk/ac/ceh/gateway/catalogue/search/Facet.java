f8a6c28d67587bbe1004eacc4fed531e305454bb
Philip Trembath <phtr@ceh.ac.uk>
2025-04-24 13:00:53 +0000

    add default admin state to Facet

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/search/Facet.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/search/Facet.java
@@ -21,4 +21,8 @@ public class Facet {
         this.results = (results != null) ? results : new ArrayList<>();
     }
 
+    public static class FacetBuilder {
+        private boolean admin = false;
+    }
+
 }
