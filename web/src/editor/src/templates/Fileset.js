9d5c380d171892bfc9fc3c6b3b314232c695d93b
Philip Trembath <phtr@ceh.ac.uk>
2025-05-20 11:29:33 +0000

    minor ui fixes

--- web/src/editor/src/templates/Fileset.js
+++ web/src/editor/src/templates/Fileset.js
@@ -3,28 +3,28 @@ import _ from 'underscore'
 export default _.template(`
   <div class="row">
       <div class="col-lg-2">
-          <label class="control-label">Fileset Name</label>
+          <label class="control-label">Fileset</label>
       </div>
       <div class="col-lg-4">
-          <input data-name='filesetName' class="editor-input" value="<%= data.filesetName %>">
+          <input data-name='filesetName' placeholder="e.g., csv files" class="editor-input" value="<%= data.filesetName %>">
       </div>
       <div class="col-lg-2">
-          <label class="control-label">Encoding Format</label>
+          <label class="control-label">Encoding format</label>
       </div>
       <div class="col-lg-4">
-          <input data-name='encodingFormat' class="editor-input" value="<%= data.encodingFormat %>">
+          <input data-name='encodingFormat' class="editor-input" placeholder="e.g., text/csv" value="<%= data.encodingFormat %>">
       </div>
   </div>
   <div class="row">
       <div class="col-lg-2">
-          <label class="control-label">Includes</label>
+          <label class="control-label">Filename pattern</label>
       </div>
       <div class="col-lg-4">
-          <input data-name='includes' class="editor-input" value="<%= data.includes %>">
+          <input data-name='includes' placeholder="e.g., *.csv" class="editor-input" value="<%= data.includes %>">
       </div>
       <div class="col-lg-6">
         <button class="editor-button dropdown-toggle" data-bs-toggle="dropdown" type="button" id="addObservedPropertyBtn" aria-expanded="false">
-            Add Observed Property
+            Add property
         </button>
         <ul class="dropdown-menu" aria-labelledby="addObservedPropertyBtn"></ul>
       </div>
