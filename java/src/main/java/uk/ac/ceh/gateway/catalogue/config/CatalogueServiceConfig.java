365fbeab6a1f1bbe9f5e70ac5fb62f9a5f178ae6
Philip Trembath <phtr@ceh.ac.uk>
2026-04-15 12:55:17 +0000

    added data format to solr index

--- java/src/main/java/uk/ac/ceh/gateway/catalogue/config/CatalogueServiceConfig.java
+++ java/src/main/java/uk/ac/ceh/gateway/catalogue/config/CatalogueServiceConfig.java
@@ -88,6 +88,7 @@ public class CatalogueServiceConfig {
                 .logo("eidc.png")
                 .facetKey("topic")
                 .facetKey("recordType")
+                .facetKey("dataFormat")
                 .facetKey("rightsHolder")
                 .facetKey("funder")
                 .facetKey("status")
