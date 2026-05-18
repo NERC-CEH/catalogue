bb108afa4abd73ca70e1f2ad5e17af42b52c3813
Rod Scott <rjsc@ceh.ac.uk>
2026-05-18 13:28:17 +0100

    Fix web asset setup in Docker dev workflow
    - web service: run npm install before starting watchers so a clean
      checkout works without a host-side prerequisite
    - entrypoint-dev.sh: use -L (symlink test) instead of -e (existence
      test) and ln -sf so broken symlinks from a previous crashed container
      are replaced rather than causing a startup abort

--- docker/entrypoint-dev.sh
+++ docker/entrypoint-dev.sh
@@ -3,12 +3,12 @@ set -e
 
 mkdir -p /app/static /app/mapfiles /app/metrics-db /app/dropbox
 
-[ -e /app/static/img ]        || ln -sr /app/web/img /app/static/
-[ -e /app/static/css ]        || ln -sr /app/web/css /app/static/
-[ -e /app/static/scripts ]    || ln -sr /app/web/dist /app/static/scripts
-[ -e /app/static/css/images ] || ln -sr \
+[ -L /app/static/img ]        || ln -sf /app/web/img /app/static/img
+[ -L /app/static/css ]        || ln -sf /app/web/css /app/static/css
+[ -L /app/static/scripts ]    || ln -sf /app/web/dist /app/static/scripts
+[ -L /app/static/css/images ] || ln -sf \
     /app/web/node_modules/leaflet-draw/dist/images /app/static/css/images
-[ -e /app/static/webfonts ]   || ln -sr \
+[ -L /app/static/webfonts ]   || ln -sf \
     /app/web/node_modules/@fortawesome/fontawesome-free/webfonts /app/static/webfonts
 
 exec ./gradlew :java:bootRun "$@"
