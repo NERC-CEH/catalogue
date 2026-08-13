a4740ac997a60cddb1dbf0487b5a98608b8471e5
Philip Trembath <phtr@ceh.ac.uk>
2026-08-13 11:36:43 +0000

    Revert "Fix JavaScript style"
    This reverts commit 713cd74108e4a5b202973805ac88edd84d4d3f52.

--- web/src/editor/src/views/RelationshipView.js
+++ web/src/editor/src/views/RelationshipView.js
@@ -185,6 +185,7 @@ export default ObjectInputView.extend({
       autocompleteInput.attr('placeholder', enabled ? 'Enter record ID or type to search…' : 'Choose a relationship type first')
     })
 
+
     if (this.existingRecord) {
       const infoString =
         await generateInformationString(this.model.get('target'))
